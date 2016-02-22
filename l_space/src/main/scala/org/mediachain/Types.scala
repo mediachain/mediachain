539a72d31b4c7a8dc6d653edc818cdfb47fcbc20
Yusef Napora <yusef@napora.org>
2016-02-22 14:30:28 -0500

    make id optional for vertex case classes so Orient doesn't blow up

--- l_space/src/main/scala/org/mediachain/Types.scala
+++ l_space/src/main/scala/org/mediachain/Types.scala
@@ -8,17 +8,17 @@ object Types {
   val AuthoredBy  = "authored-by"
 
   @label("Canonical")
-  case class Canonical(@id id: String,
+  case class Canonical(@id id: Option[String],
                        canonicalID: String)
 
   object Canonical {
     def create(): Canonical = {
-      Canonical("", UUID.randomUUID.toString)
+      Canonical(None, UUID.randomUUID.toString)
     }
 
     def apply(v: Vertex): Canonical = {
       Canonical(
-        v.value[String]("id"),
+        v.valueOption[String]("id"),
         v.value[String]("canonicalID")
       )
     }
@@ -31,15 +31,19 @@ object Types {
                              blob: String) extends MetadataBlob
 
   @label("Person")
-  case class Person(@id id: String,
+  case class Person(@id id: Option[String],
                     name: String) extends MetadataBlob
 
   object Person {
+    def create(name: String) = {
+      Person(None, name)
+    }
+
     def apply(v: Vertex): Option[Person] = {
       if (v.label() == "Person") {
         Some(
           Person(
-            id = v.value("id"),
+            id = v.valueOption[String]("id"),
             name = v.value("name")
           )
         )
@@ -50,10 +54,28 @@ object Types {
   }
 
   @label("PhotoBlob")
-  case class PhotoBlob(@id id: String,
+  case class PhotoBlob(@id id: Option[String],
                        title: String,
                        description: String,
                        date: String,
                        author: Option[Person]) extends MetadataBlob
+
+  object PhotoBlob {
+    def apply(v: Vertex): Option[PhotoBlob] = {
+      if (v.label() == "PhotoBlob") {
+        Some(
+          PhotoBlob(
+            id = v.valueOption[String]("id"),
+            title = v.value("title"),
+            description = v.value("description"),
+            date = v.value("date"),
+            author = None // FIXME(yusef)
+          )
+        )
+      } else {
+        None
+      }
+    }
+  }
 }
 
