03b1591cdb008c73b2d934f8cecc348900f2a8d0
Cole Brown <bigswim@gmail.com>
2016-02-22 12:33:58 -0500

    WIP

--- l_space/src/main/scala/org/mediachain/Types.scala
+++ l_space/src/main/scala/org/mediachain/Types.scala
@@ -1,14 +1,59 @@
 package org.mediachain
 
 object Types {
-  type Canonical = String
+  import gremlin.scala._
+  import java.util.UUID
 
-  case class Person(id: String,
-                    name: String)
+  val DescribedBy = "described-by"
+  val AuthoredBy  = "authored-by"
 
-  case class PhotoBlob(id: String,
+  @label("Canonical")
+  case class Canonical(@id id: String,
+                       canonicalID: String)
+
+  object Canonical {
+    def create(): Canonical = {
+      Canonical("", UUID.randomUUID.toString)
+    }
+
+    def apply(v: Vertex): Canonical = {
+      Canonical(
+        v.value[String]("id"),
+        v.value[String]("canonicalID")
+      )
+    }
+  }
+
+  sealed trait MetadataBlob
+
+  @label("RawMetadataBlob")
+  case class RawMetadataBlob(@id id: String,
+                             blob: String) extends MetadataBlob
+
+  @label("Person")
+  case class Person(@id id: String,
+                    name: String) extends MetadataBlob
+
+  object Person {
+    def apply(v: Vertex): Option[Person] = {
+      if (v.label() == "Person") {
+        Some(
+          Person(
+            id = v.value("id"),
+            name = v.value("name")
+          )
+        )
+      } else {
+        None
+      }
+    }
+  }
+
+  @label("PhotoBlob")
+  case class PhotoBlob(@id id: String,
                        title: String,
                        description: String,
                        date: String,
-                       author: Option[Person])
+                       author: Option[Person]) extends MetadataBlob
 }
+
