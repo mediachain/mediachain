7df84ac0e872fe72354a39dd7423ab7613c29b62
Yusef Napora <yusef@napora.org>
2016-02-23 15:37:31 -0500

    add authored-by relationship from photoblob -> author's canonical

--- l_space/src/main/scala/org/mediachain/Ingress.scala
+++ l_space/src/main/scala/org/mediachain/Ingress.scala
@@ -1,28 +1,29 @@
 package org.mediachain
 
-import org.apache.tinkerpop.gremlin.orientdb.OrientGraph
 import org.mediachain.Types._
-import scala.util.Try
 import gremlin.scala._
 
 object Ingress {
   // throws?
   def addPerson(graph: Graph, author: Person): Canonical = {
-    val canonicalVertex = graph + Canonical.create
-    val personVertex = graph + author
+    // If there's an exact match already, return it,
+    // otherwise create a new Person vertex and canonial
+    // and return the canonical
+    Query.findPerson(graph, author).getOrElse {
+      val canonicalVertex = graph + Canonical.create
+      val personVertex = graph + author
 
-    canonicalVertex --- DescribedBy --> personVertex
+      canonicalVertex --- DescribedBy --> personVertex
 
-    Canonical(canonicalVertex)
+      Canonical(canonicalVertex)
+    }
   }
 
   def addPhotoBlob(graph: Graph, photo: PhotoBlob): Canonical = {
 
     // 1) extract author & add if they don't exist in the graph already
     val author: Option[Canonical] = photo.author.flatMap { p =>
-      Query.findPerson(graph, p).orElse {
-        Some(addPerson(graph, p))
-      }
+      Some(addPerson(graph, p))
     }
 
     // 2) check to see if a duplicate entry exists
@@ -31,6 +32,16 @@ object Ingress {
       val photoVertex = graph + photo
 
       canonicalVertex --- DescribedBy --> photoVertex
+
+      // 3) when adding a new entry with an author,
+      //    make an edge from the PhotoBlob vertex to
+      //    the Canonical vertex for the author
+      author
+        .flatMap(a => a.id)
+        .flatMap(id => graph.V(id).headOption)
+        .foreach(authorVertex => {
+          photoVertex --- AuthoredBy --> authorVertex
+        })
 
       Canonical(canonicalVertex)
     }
