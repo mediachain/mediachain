c995bc202e7e55e9664a118c6db7036f979ddd16
Yusef Napora <yusef@napora.org>
2016-03-02 15:56:07 -0500

    wip - walk directory structure and suck in json

--- translation_engine/src/main/scala/org/mediachain/translation/JsonLoader.scala
+++ translation_engine/src/main/scala/org/mediachain/translation/JsonLoader.scala
@@ -4,25 +4,47 @@ package org.mediachain.translation
 
 object JsonLoader {
 
-  import java.net.URL
+  import scala.io.Source
+  import java.io.File
+  import java.net.{URI, URL}
   import cats.data.Xor
   import org.json4s._
   import org.json4s.jackson.JsonMethods._
 
   def loadObjectFromString(jsonString: String): Xor[Throwable, JObject] = {
-    Xor.catchNonFatal {
-      parse(jsonString).asInstanceOf[JObject]
-    }
+    Xor.catchNonFatal(parse(jsonString).asInstanceOf[JObject])
   }
+
+  def loadObjectFromSource(source: Source): Xor[Throwable, JObject] = {
+    Xor.catchNonFatal(source.mkString)
+      .flatMap(loadObjectFromString)
+  }
+
 
   def loadObjectFromURL(url: URL): Xor[Throwable, JObject] = {
-    Xor.catchNonFatal {
-      import scala.io.Source
-      val source = Source.fromURL(url)
-      val contents = source.mkString
-      source.close()
-      contents
-    }.flatMap(loadObjectFromString)
+    for {
+      source <- Xor.catchNonFatal(Source.fromURL(url))
+      obj <- loadObjectFromSource(source)
+    } yield obj
   }
 
+  def loadObjectFromFile(file: File): Xor[Throwable, JObject] = {
+    for {
+      source <- Xor.catchNonFatal(Source.fromFile(file))
+      obj <- loadObjectFromSource(source)
+    } yield obj
+  }
+
+  /*
+  Doesn't compile :(
+
+  def loadObjectsFromDirectoryTree(directoryURI: URI): Xor[Throwable, Seq[JObject]] = {
+    val dir = new File(directoryURI)
+    for {
+      file <- DirectoryWalker.findWithExtension(dir, ".json")
+      obj <- loadObjectFromFile(file)
+    } yield obj
+  }
+
+  */
 }
