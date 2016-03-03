820dce29ab14b7b5c7750ca618908e39634a95cd
Yusef Napora <yusef@napora.org>
2016-03-03 15:03:16 -0500

    test artwork translation

--- translation_engine/src/test/scala/org/mediachain/translation/SpecResources.scala
+++ translation_engine/src/test/scala/org/mediachain/translation/SpecResources.scala
@@ -6,4 +6,12 @@ object SpecResources {
   lazy val simpleTestResourceUrl = resourceUrl("/simple-test.json")
   lazy val tateSampleDirResourceUrl = resourceUrl("/tate-sample")
   val numTateSamples = 18
+
+  object TateSampleArtworkA00001 {
+    lazy val url = resourceUrl("/tate-sample/artworks/a/000/a00001-1035.json")
+    val title = "A Figure Bowing before a Seated Old Man with his Arm Outstretched in Benediction. Verso: Indecipherable Sketch"
+    val medium = "Watercolour, ink, chalk and graphite on paper. Verso: graphite on paper"
+    val dateText = "date not known"
+    val artistName = "Robert Blake"
+  }
 }
--- translation_engine/src/test/scala/org/mediachain/translation/TateTranslatorSpec.scala
+++ translation_engine/src/test/scala/org/mediachain/translation/TateTranslatorSpec.scala
@@ -11,9 +11,24 @@ object TateTranslatorSpec extends Specification {
   def is =
   s2"""
        $loadsArtwork - Translates Tate artwork json into PhotoBlob + List[Person]
+       $loadsArtworksFromDir - Translates all artworks from a directory structure
     """
 
   def loadsArtwork = {
+    val expected = SpecResources.TateSampleArtworkA00001
+    val jsonResult = JsonLoader.loadObjectFromURL(expected.url)
+    val translated = jsonResult.flatMap(SUT.loadArtwork)
+      .getOrElse(throw new Exception("Unable to translate sample artwork from tate collection"))
+
+    val (blob, people) = translated
+
+    (blob.title must_== expected.title) and
+      (blob.date must_== expected.dateText) and
+      (blob.author must beSome[Person].which(_.name == expected.artistName)) and
+      (blob.author must_== people.headOption)
+  }
+
+  def loadsArtworksFromDir = {
 
     val tateSampleDirURI = SpecResources.tateSampleDirResourceUrl.toURI
     val jsonResults = JsonLoader.loadObjectsFromDirectoryTree(tateSampleDirURI)
