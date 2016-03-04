24b8c34837195bc9a45a0a9bd919b887fd3ccaa1
Yusef Napora <yusef@napora.org>
2016-03-04 12:40:19 -0500

    skip tate tests if sample data doesn't exist
    make ignored dir for test data: `translation_engine/test-resources/datasets`

--- translation_engine/src/test/scala/org/mediachain/translation/JsonLoaderSpec.scala
+++ translation_engine/src/test/scala/org/mediachain/translation/JsonLoaderSpec.scala
@@ -9,21 +9,10 @@ object JsonLoaderSpec extends Specification {
   def is =
     s2"""
         Loads json from a URL into an AST $loadsFromURL
-        Loads all json objects in a directory structure: $loadsFromDir
       """
 
   def loadsFromURL = {
     val json = JsonLoader.loadObjectFromURL(SpecResources.simpleTestResourceUrl)
     json.toEither must beRight
-  }
-
-
-  def loadsFromDir = {
-    val tateSampleDirURI = SpecResources.tateSampleDirResourceUrl.toURI
-    val jsonResults = JsonLoader.loadObjectsFromDirectoryTree(tateSampleDirURI)
-    val asEithers = jsonResults.map(_.toEither)
-
-    (asEithers.size must_== SpecResources.numTateSamples) and
-      (asEithers must contain(beRight[JObject]))
   }
 }
