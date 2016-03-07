b0fba1736224f516100dc8eb4459d89cb736af60
Cole Brown <bigswim@gmail.com>
2016-03-07 16:14:54 -0500

    Add `which`-like clause support to XorMatchers

--- l_space/src/test/scala/org.mediachain/XorMatchers.scala
+++ l_space/src/test/scala/org.mediachain/XorMatchers.scala
@@ -1,41 +1,34 @@
 package org.mediachain
 
 import org.specs2.matcher.Matcher
+import org.specs2.execute.{Failure, Result}
 import org.specs2.matcher.MatchersImplicits._
 import cats.data.Xor
 
 trait XorMatchers {
-  def beRight[T](validator: (T => Boolean)): Matcher[Xor[_, T]] = {
+  def beRightXor[T](checker: (T => Result)): Matcher[Xor[_, T]] = {
     (x: Xor[_, T]) => x match {
-      case container@Xor.Right(value) =>
-        (validator(value), s"$container did not match validator")
+      case Xor.Right(value) =>
+        checker(value)
       case _ =>
-        (false, "Xor value was instance of Xor.Left")
+        new Failure(s"Value $x was instance of Xor.Left")
     }
   }
 
-  def beRight[T](value: T): Matcher[Xor[_, T]] = beRight {
-    (x: T) => x == value
-  }
-
-  def beRight(): Matcher[Xor[_, _]] = { (x: Xor[_, _]) =>
+  def beRightXor(): Matcher[Xor[_, _]] = { (x: Xor[_, _]) =>
     (x.isRight, "Xor value was instance of Xor.Left")
   }
 
-  def beLeft[T](validator: (T => Boolean)): Matcher[Xor[T, _]] = {
+  def beLeftXor[T](checker: (T => Result)): Matcher[Xor[T, _]] = {
     (x: Xor[T, _]) => x match {
-      case container@Xor.Left(value) =>
-        (validator(value), s"$container did not match validator")
+      case Xor.Left(value) =>
+        checker(value)
       case _ =>
-        (false, "Xor value was instance of Xor.Right")
+        new Failure(s"Value $x was instance of Xor.Right")
     }
   }
 
-  def beLeft[T](value: T): Matcher[Xor[T, _]] = beLeft {
-    (x: T) => x == value
-  }
-
-  def beLeft(): Matcher[Xor[_, _]] = { (x: Xor[_, _]) =>
+  def beLeftXor(): Matcher[Xor[_, _]] = { (x: Xor[_, _]) =>
     (x.isLeft, "Xor value was instance of Xor.Right")
   }
 }
