5d1ed4d3c9535c1c2e3943c0e05dfd18dcdf2acf
Yusef Napora <yusef@napora.org>
2016-03-10 16:32:59 -0500

    Add implict `toGraphsonString` and `printGraphson` methods to `Graph`, `Vertex` and `Edge`

--- l_space/src/main/scala/org/mediachain/io/GraphJsonWriter.scala
+++ l_space/src/main/scala/org/mediachain/io/GraphJsonWriter.scala
@@ -12,6 +12,48 @@ import gremlin.scala._
 
 object GraphJsonWriter {
 
+  implicit class GraphIOImplicits(graph: Graph) {
+    lazy val writer = graph.io(IoCore.graphson).writer.create
+
+    def toGraphsonString: String = {
+      val out = new ByteArrayOutputStream
+      writer.writeGraph(out, graph)
+      out.toString("UTF-8")
+    }
+
+    def printGraphson(): Unit = {
+      writer.writeGraph(System.out, graph)
+    }
+  }
+
+  implicit class VertexIOImplicits(vertex: Vertex) {
+    lazy val writer = vertex.graph.io(IoCore.graphson).writer.create
+
+    def toGraphsonString(edgeDirection: Direction = Direction.BOTH): String = {
+      val out = new ByteArrayOutputStream
+      writer.writeVertex(out, vertex, edgeDirection)
+      out.toString("UTF-8")
+    }
+
+    def printGraphson(edgeDirection: Direction = Direction.BOTH): Unit = {
+      writer.writeVertex(System.out, vertex, edgeDirection)
+    }
+  }
+
+  implicit class EdgeIOImplicits(edge: Edge) {
+    lazy val writer = edge.graph.io(IoCore.graphson).writer.create
+
+    def toGraphsonString: String = {
+      val out = new ByteArrayOutputStream
+      writer.writeEdge(out, edge)
+      out.toString("UTF-8")
+    }
+
+    def printGraphson(): Unit = {
+      writer.writeEdge(System.out, edge)
+    }
+  }
+
   // TOOD: error handling
   def toGraphsonVertexObjects(graph: Graph): Iterator[JObject] = {
     val writer = graph.io(IoCore.graphson).writer.create
