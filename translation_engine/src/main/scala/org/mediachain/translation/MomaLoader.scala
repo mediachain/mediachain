913615a1309c548583d3d5ef25e572abdf05febe
Cole Brown <bigswim@gmail.com>
2016-03-14 18:35:46 -0400

    Comment MomaLoader module

--- translation_engine/src/main/scala/org/mediachain/translation/MomaLoader.scala
+++ translation_engine/src/main/scala/org/mediachain/translation/MomaLoader.scala
@@ -10,10 +10,22 @@ import cats.implicits._
 import org.mediachain.Types.{Person, PhotoBlob}
 
 object MomaLoader {
+  /** A convenience case class for parsing JSON blobs into a more abstract form.
+    *
+    * @param Title Title of the work
+    * @param Medium Medium of the work (becomes description for now)
+    * @param Date Date of the work
+    * @param Artist Artist of the work
+    */
   case class MomaPhotoBlob(Title: String,
                            Medium: String,
                            Date: String,
                            Artist: String) {
+
+    /** Convenience method that casts a MomaPhotoBlob to a PhotoBlob
+      *
+      * @return
+      */
     def asPhotoBlob: PhotoBlob = PhotoBlob(
       id = None,
       title = Title,
@@ -23,11 +35,23 @@ object MomaLoader {
     )
   }
 
+  /** Casts a JValue to a PhotoBlob using `extract` and `asPhotoBlob`
+    *
+    * @param json The JValue representing a work
+    * @return A `PhotoBlob` extracted from the JValue
+    */
   def jsonToPhotoBlob(json: JValue): PhotoBlob = {
     implicit val formats = DefaultFormats
     json.extract[MomaPhotoBlob].asPhotoBlob
   }
 
+  /** Assert the presence of a token & advance to the `nextToken` upon success.
+    * Upon failure, return an error.
+    *
+    * @param parser The current parser
+    * @param token The token to search for
+    * @return Unit or an error
+    */
   def parseToken(parser: JsonParser, token: JsonToken): Xor[String, Unit] = {
     val curToken = parser.getCurrentToken
     if (curToken == token) {
@@ -38,6 +62,16 @@ object MomaLoader {
     }
   }
 
+  /** The main workhorse. Parses a JArray, emitting individual objects as they
+    * are parsed in a `Streaming`. The objects on the stream are `Xor`'s, to
+    * help localize which object a streaming parse fails on, should it fail.
+    *
+    * In the event of a failure, the parse will stop and the end of the stream
+    * will be an Xor value containing the error message.
+    *
+    * @param parser The current parser
+    * @return A `Streaming` of either `JValue`s or `String` error messages
+    */
   def parseJArray(parser: JsonParser):
   Streaming[Xor[String, JValue]] = {
     def helper: Streaming[Xor[String, JValue]] = {
@@ -46,7 +80,7 @@ object MomaLoader {
       } else {
         parseJValue(parser) match {
           case r@Xor.Right(_) => r %:: helper
-          case l => l %:: Streaming.empty[Xor[String, JValue]]
+          case l => Streaming(l)
         }
       }
     }
@@ -57,6 +91,12 @@ object MomaLoader {
     }
   }
 
+  /** Parses an array of objects strictly, rather than lazily. This is used for
+    * parsing arrays within the objects held by the main JArray we are parsing.
+    *
+    * @param parser The current parser
+    * @return Either a `JArray` or an error message
+    */
   def parseSubJArray(parser: JsonParser): Xor[String, JArray] = {
     def helper(results: List[JValue]): Xor[String, List[JValue]] = {
       if (parser.getCurrentToken == JsonToken.END_ARRAY) {
@@ -72,6 +112,12 @@ object MomaLoader {
     } yield JArray(objects)
   }
 
+  /** Parses a single value, reflecting off of the current token in the parse.
+    *
+    * @param parser The current parser
+    * @return The `JValue` under the parse cursor, or an error if the parse is
+    *         not currently at a JValue.
+    */
   def parseJValue(parser: JsonParser): Xor[String, JValue] = {
     parser.getCurrentToken match {
       case JsonToken.START_OBJECT => parseJOBject(parser)
@@ -87,6 +133,11 @@ object MomaLoader {
     }
   }
 
+  /** Parses a tuple (fieldName: String, fieldValue: JValue)
+    *
+    * @param parser The current parser
+    * @return A `JField` or an error message
+    */
   def parseField(parser: JsonParser): Xor[String, JField] = {
     for {
       _ <- parseToken(parser, JsonToken.FIELD_NAME)
@@ -98,6 +149,11 @@ object MomaLoader {
     }
   }
 
+  /** Strictly parse a list of `JField`s.
+    *
+    * @param parser The current parser
+    * @return A list of `JField`s or an error message
+    */
   def parseFields(parser: JsonParser): Xor[String, List[JField]] = {
     def helper(results: List[JField]): Xor[String, List[JField]] = {
       if (parser.getCurrentToken == JsonToken.FIELD_NAME) {
@@ -110,6 +166,11 @@ object MomaLoader {
     helper(List())
   }
 
+  /** Parse a `JObject`
+    *
+    * @param parser The current parser
+    * @return A `JObject` or an error message
+    */
   def parseJOBject(parser: JsonParser): Xor[String, JObject] = {
     for {
       _      <- parseToken(parser, JsonToken.START_OBJECT)
@@ -118,6 +179,12 @@ object MomaLoader {
     } yield JObject(fields)
   }
 
+  /** Given a filename representing a MoMA-schema list of artworks, parse the
+    * file into a stream of `PhotoBlob`s.
+    *
+    * @param filename The filename to read and parse
+    * @return A stream of `PhotoBlob`s
+    */
   def loadPhotoBlobs(filename: String): Streaming[PhotoBlob] = {
     val jf = new JsonFactory
     val parser = jf.createParser(new File(filename))
