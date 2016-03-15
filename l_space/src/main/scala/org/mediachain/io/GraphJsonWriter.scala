d094cade371ef3cb80394f8bfae341cda2d243ee
Yusef Napora <yusef@napora.org>
2016-03-15 13:51:48 -0400

    Add Cbor methods to render to byte array, base64 string

---
+++ l_space/src/main/scala/org/mediachain/io/GraphJsonWriter.scala
@@ -0,0 +1,27 @@
+package org.mediachain.io
+
+import java.io._
+
+import org.apache.tinkerpop.gremlin.structure.Direction
+import org.apache.tinkerpop.gremlin.structure.io.IoCore
+import org.json4s.JObject
+import org.json4s.jackson.{JsonMethods => Json}
+import scala.collection.JavaConversions._
+
+import gremlin.scala._
+
+object GraphJsonWriter {
+
+  // TOOD: error handling
+  def toGraphsonVertexObjects(graph: Graph): Iterator[JObject] = {
+    val writer = graph.io(IoCore.graphson).writer.create
+    val verts: Iterator[Vertex] = graph.V.toStream.iterator
+
+    verts.map { v =>
+      val out = new ByteArrayOutputStream()
+      writer.writeVertex(out, v, Direction.BOTH)
+      val in = new ByteArrayInputStream(out.toByteArray)
+      Json.parse(in).asInstanceOf[JObject]
+    }
+  }
+}
