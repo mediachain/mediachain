32517d97597c6b5cc3de79d4d9ac2872f0779f96
Arkadiy Kukarkin <arkadiy@mine.nyc>
2016-03-16 15:54:10 -0400

    Dispatcher for different translation engines

--- translation_engine/src/main/scala/io/mediachain/translation/Translator.scala
+++ translation_engine/src/main/scala/io/mediachain/translation/Translator.scala
@@ -1,6 +1,28 @@
 package io.mediachain.translation
 
+import io.mediachain.Types.PhotoBlob
+import org.apache.tinkerpop.gremlin.orientdb.OrientGraphFactory
+import cats.data.Streaming
+
 trait Translator {
   val name: String
   val version: Int
+  def loadPhotoBlobs(filename: String): Streaming[PhotoBlob]
+}
+
+object TranslatorDispatcher {
+  def dispatch(partner: String, path: String) = {
+    //println("partner: " + partner + ", path: " + path)
+    val translator: Translator = partner match {
+      case "moma" => moma.MomaTranslator
+      case "tate" => tate.TateTranslator
+    }
+
+    val blobS = translator.loadPhotoBlobs(path)
+
+    val url = sys.env.get("ORIENTDB_URL").getOrElse(throw new Exception("ORIENTDB_URL required"))
+    val user = sys.env.get("ORIENTDB_USER").getOrElse(throw new Exception("ORIENTDB_USER required"))
+    val password = sys.env.get("ORIENTDB_PASSWORD").getOrElse(throw new Exception("ORIENTDB_PASSWORD required"))
+    val graph = new OrientGraphFactory(url, user, password).getNoTx()
+  }
 }
