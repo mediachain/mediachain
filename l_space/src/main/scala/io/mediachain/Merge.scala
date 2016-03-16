9911037a258c74da5d8aa53bac0519efdc1a1bc7
Yusef Napora <yusef@napora.org>
2016-03-16 12:51:37 -0400

    first impl of mergeCanonicals

---
+++ l_space/src/main/scala/io/mediachain/Merge.scala
@@ -0,0 +1,25 @@
+package io.mediachain
+
+import cats.data.Xor
+import gremlin.scala._
+import io.mediachain.Types._
+
+object Merge {
+
+  def mergeCanonicals(graph: Graph, child: Canonical, parent: Canonical)
+  : Xor[GraphError, Canonical] = {
+
+    for {
+      childV <- child.vertex(graph)
+      parentV <- parent.vertex(graph)
+      childRootBlobEdges = childV.outE(DescribedBy).toList
+      childRootBlobVs = childRootBlobEdges.map(_.outVertex)
+    } yield {
+      childRootBlobEdges.foreach(_.setProperty(Keys.Deprecated, true))
+      childRootBlobVs.foreach(v => parentV --- DescribedBy --> v)
+      childV --- SupersededBy --> parentV
+      parent
+    }
+  }
+
+}
