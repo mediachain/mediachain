633af28cd12571151109eee51ae4c451624d3dfd
Arkadiy Kukarkin <arkadiy@mine.nyc>
2016-03-17 13:41:17 -0400

    Redefine loaders as stateful
    - FSloader now has two iterator for raw + parsed json objects so
      whichever one we produce we can always get the other
    - Loaders now exist on a per-path basis and keep their state
    - TODO: port MoMA

--- translation_engine/src/main/scala/io/mediachain/translation/Translator.scala
+++ translation_engine/src/main/scala/io/mediachain/translation/Translator.scala
@@ -1,27 +1,45 @@
 package io.mediachain.translation
 
 import java.io.File
+import io.mediachain.translation.TranslationError.ParsingFailed
+
 import scala.io.Source
 import cats.data.Xor
 import io.mediachain.Types.{RawMetadataBlob, PhotoBlob}
 import org.apache.tinkerpop.gremlin.orientdb.OrientGraphFactory
+import org.json4s._
+import com.fasterxml.jackson.core.JsonFactory
 
 trait Translator {
   val name: String
   val version: Int
-  def translate(source: String): Xor[TranslationError, (PhotoBlob, RawMetadataBlob)]
+  def translate(source: JObject): Xor[TranslationError, PhotoBlob]
 }
 
 trait FSLoader {
-  def loadPhotoBlobs(path: String): Iterator[Xor[TranslationError,(PhotoBlob, RawMetadataBlob)]]
+  def loadPhotoBlobs: Iterator[(Xor[TranslationError,PhotoBlob], RawMetadataBlob)]
+  val rawI: Iterator[String]
+  val jsonI: Iterator[Xor[TranslationError, JObject]]
 }
 
 trait DirectoryWalkerLoader extends FSLoader {
   self: Translator =>
-  def loadPhotoBlobs(path: String): Iterator[Xor[TranslationError,(PhotoBlob, RawMetadataBlob)]] = {
-    val files = DirectoryWalker.findWithExtension(new File(path), ".json")
-    val jsonStrings = files.map(Source.fromFile(_).mkString)
-    jsonStrings.map(translate).iterator
+  val path: String
+  val fileI: Iterator[File] = DirectoryWalker.findWithExtension(new File(path), ".json").iterator
+  val rawI = fileI.map(Source.fromFile(_).mkString)
+  val jsonI = fileI.map { file =>
+      val jf = new JsonFactory
+      val parser = jf.createParser(file)
+      JsonLoader.parseJOBject(parser)
+        .leftMap(err =>
+          TranslationError.ParsingFailed(new RuntimeException(err)))
+    }
+
+  def loadPhotoBlobs: Iterator[(Xor[TranslationError,PhotoBlob], RawMetadataBlob)] = {
+    val combinedI: Iterator[(Xor[ParsingFailed, JObject], String)] = jsonI.zip(rawI)
+    combinedI.map { case (jsonXor, raw) =>
+      (jsonXor.flatMap(translate), RawMetadataBlob(None, raw))
+    }
   }
 }
 
@@ -30,10 +48,10 @@ object TranslatorDispatcher {
     //println("partner: " + partner + ", path: " + path)
     val translator: FSLoader = partner match {
       //case "moma" => moma.MomaTranslator
-      case "tate" => tate.TateLoader
+      case "tate" => new tate.TateLoader(path)
     }
 
-    val blobS = translator.loadPhotoBlobs(path)
+    val blobS = translator.loadPhotoBlobs
 
     val url = sys.env.get("ORIENTDB_URL").getOrElse(throw new Exception("ORIENTDB_URL required"))
     val user = sys.env.get("ORIENTDB_USER").getOrElse(throw new Exception("ORIENTDB_USER required"))
--- translation_engine/src/main/scala/io/mediachain/translation/moma/MomaTranslator.scala
+++ translation_engine/src/main/scala/io/mediachain/translation/moma/MomaTranslator.scala
@@ -12,61 +12,61 @@ import io.mediachain.translation.JsonLoader.parseJArray
 import io.mediachain.translation.{TranslationError, Translator}
 
 import scala.util.{Try, Success, Failure}
-
-object MomaTranslator extends Translator {
-  val name = "MomaCollectionTranslator"
-  val version = 1
-
-  /** A convenience case class for parsing JSON blobs into a more abstract form.
-    *
-    * @param Title Title of the work
-    * @param Medium Medium of the work (becomes description for now)
-    * @param Date Date of the work
-    * @param Artist Artist of the work
-    */
-  private case class MomaPhotoBlob(Title: String,
-                           Medium: String,
-                           Date: String,
-                           Artist: String) {
-    def asPhotoBlob: PhotoBlob = PhotoBlob(
-      id = None,
-      title = Title,
-      description = Medium,
-      date = Date,
-      author = Some(Person(id = None, Artist))
-    )
-  }
-
-  /** Casts a JValue to a PhotoBlob using `extract` and `asPhotoBlob`
-    *
-    * @param json The JValue representing a work
-    * @return A `PhotoBlob` extracted from the JValue
-    */
-  def jsonToPhotoBlobTuple(json: JObject): Xor[TranslationError, (PhotoBlob, RawMetadataBlob)] = {
-    implicit val formats = DefaultFormats
-    val rawBlob = RawMetadataBlob(None, write(json))
-    Try(json.extract[MomaPhotoBlob].asPhotoBlob) match {
-      case Success(blob) => Xor.right((blob, rawBlob))
-      case Failure(exn)  => Xor.left(ParsingFailed(exn))
-    }
-  }
-
-  /** Given a filename representing a MoMA-schema list of artworks, parse the
-    * file into a stream of `PhotoBlob`s.
-    *
-    * @param path The filename to read and parse
-    * @return A stream of `PhotoBlob`s
-    */
-  def loadPhotoBlobs(path: String): Iterator[Xor[TranslationError, (PhotoBlob, RawMetadataBlob)]] = {
-    val jf = new JsonFactory
-    val parser = jf.createParser(new File(path))
-
-    parser.nextToken
-
-    parseJArray(parser).flatMap {
-      case Xor.Right(json: JObject) => List(jsonToPhotoBlobTuple(json))
-      case _ => List.empty
-    }
-  }
-}
+//
+//object MomaTranslator extends Translator {
+//  val name = "MomaCollectionTranslator"
+//  val version = 1
+//
+//  /** A convenience case class for parsing JSON blobs into a more abstract form.
+//    *
+//    * @param Title Title of the work
+//    * @param Medium Medium of the work (becomes description for now)
+//    * @param Date Date of the work
+//    * @param Artist Artist of the work
+//    */
+//  private case class MomaPhotoBlob(Title: String,
+//                           Medium: String,
+//                           Date: String,
+//                           Artist: String) {
+//    def asPhotoBlob: PhotoBlob = PhotoBlob(
+//      id = None,
+//      title = Title,
+//      description = Medium,
+//      date = Date,
+//      author = Some(Person(id = None, Artist))
+//    )
+//  }
+//
+//  /** Casts a JValue to a PhotoBlob using `extract` and `asPhotoBlob`
+//    *
+//    * @param json The JValue representing a work
+//    * @return A `PhotoBlob` extracted from the JValue
+//    */
+//  def jsonToPhotoBlobTuple(json: JObject): Xor[TranslationError, (PhotoBlob, RawMetadataBlob)] = {
+//    implicit val formats = DefaultFormats
+//    val rawBlob = RawMetadataBlob(None, write(json))
+//    Try(json.extract[MomaPhotoBlob].asPhotoBlob) match {
+//      case Success(blob) => Xor.right((blob, rawBlob))
+//      case Failure(exn)  => Xor.left(ParsingFailed(exn))
+//    }
+//  }
+//
+//  /** Given a filename representing a MoMA-schema list of artworks, parse the
+//    * file into a stream of `PhotoBlob`s.
+//    *
+//    * @param path The filename to read and parse
+//    * @return A stream of `PhotoBlob`s
+//    */
+//  def loadPhotoBlobs(path: String): Iterator[Xor[TranslationError, (PhotoBlob, RawMetadataBlob)]] = {
+//    val jf = new JsonFactory
+//    val parser = jf.createParser(new File(path))
+//
+//    parser.nextToken
+//
+//    parseJArray(parser).flatMap {
+//      case Xor.Right(json: JObject) => List(jsonToPhotoBlobTuple(json))
+//      case _ => List.empty
+//    }
+//  }
+//}
 
