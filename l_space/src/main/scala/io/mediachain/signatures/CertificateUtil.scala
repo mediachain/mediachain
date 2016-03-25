da5028720b332964f9623d0f266765aa6597a3aa
Yusef Napora <yusef@napora.org>
2016-03-25 13:43:55 -0400

    Use oracle's X500Name implementation for easy Common Name extraction
    - This is much simpler than digging into bouncy castle's
      internal representation of the common name
    - But, it ties us to the Oracle JVM, since the sun.security
      package isn't included in OpenJDK, etc.

--- l_space/src/main/scala/io/mediachain/signatures/CertificateUtil.scala
+++ l_space/src/main/scala/io/mediachain/signatures/CertificateUtil.scala
@@ -1,54 +1,24 @@
 package io.mediachain.signatures
 
+import java.io.IOException
 import java.security.cert.X509Certificate
 import javax.security.auth.x500.X500Principal
 
 import cats.data.Xor
 import io.mediachain.core.SignatureError.CertificateLacksCommonName
-import org.bouncycastle.asn1._
-import org.bouncycastle.asn1.x500.{RDN, X500Name}
-import org.bouncycastle.asn1.x500.style.{BCStyle, IETFUtils}
+import sun.security.x509.X500Name
 
 object CertificateUtil {
-
-  /**
-    * Try to extract just the "value" portion of the given `ASN1Object`
-    * Used to get a human readable "Common Name" out of an RDN object.
-    * The RDN's primitive representation is basically a Set of Vectors
-    * of two elements, the first being the "oid" of the field name, and
-    * the second being the value as a string.  If we just call the
-    * seemingly helpful `IETFUtils.valueToString` method, it will encode
-    * the whole thing, e.g. `[["0.1.2.3", "foo.bar.com"]]`
-    *
-    */
-  private def decode(rdn: RDN): String = {
-    val maybeDecoded: Option[String] =
-      try {
-        val set = rdn.toASN1Primitive.asInstanceOf[DERSet]
-        val seq = set.getObjectAt(0).asInstanceOf[DERSequence]
-        val str = seq.getObjectAt(1).asInstanceOf[DERUTF8String]
-        Some(str.getString)
-      } catch {
-        case _: Throwable => None
-      }
-    // fallback to `valueToString`, if things fail
-    maybeDecoded.getOrElse(IETFUtils.valueToString(rdn))
-  }
-
 
   def canonicalName(cert: X509Certificate): String =
     cert.getSubjectX500Principal.getName(X500Principal.CANONICAL)
 
 
-  def commonNames(cert: X509Certificate): List[String] = {
-    val name = new X500Name(cert.getSubjectX500Principal.getName)
-    val rdns = name.getRDNs(BCStyle.CN)
-    rdns.map(decode).toList
-  }
-
-
   def commonName(cert: X509Certificate)
   : Xor[CertificateLacksCommonName, String] =
-    Xor.fromOption(commonNames(cert).headOption, CertificateLacksCommonName())
+    Xor.catchOnly[IOException] {
+      val name = new X500Name(cert.getSubjectX500Principal.getName)
+      name.getCommonName
+    }.leftMap(_ => CertificateLacksCommonName())
 
 }
