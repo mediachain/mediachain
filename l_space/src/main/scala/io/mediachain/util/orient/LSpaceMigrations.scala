a87afdc5b7444d6ae50e63adec7d66bc6093e484
Yusef Napora <yusef@napora.org>
2016-03-28 11:04:36 -0400

    Set orient type of signatures field to EMBEDDEDMAP
    this required some conversions in the marshaller, since orient will
    only accept a java.util.Map as the value for an EMBEDDEDMAP field.

--- l_space/src/main/scala/io/mediachain/util/orient/LSpaceMigrations.scala
+++ l_space/src/main/scala/io/mediachain/util/orient/LSpaceMigrations.scala
@@ -4,6 +4,7 @@ import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx
 import springnz.orientdb.migration.{Migration, ODBMigrations}
 import springnz.orientdb.session.ODBSession
 import io.mediachain.Types._
+import springnz.orientdb.ODBScala
 
 class LSpaceMigrations extends ODBMigrations with OrientSchema {
 
@@ -48,7 +49,28 @@ class LSpaceMigrations extends ODBMigrations with OrientSchema {
       () // explicitly return unit
     }
 
+  def migration1: ODBSession[Unit] =
+    ODBSession {implicit db: ODatabaseDocumentTx =>
+
+      val signaturesProp = MapProperty("signatures")
+        .mandatory(true)
+        .defaultValue(Map[String,String]())
+
+
+      val classNames =
+        List("Canonical", "PhotoBlob", "Person", "RawMetadataBlob")
+        .map("V_" + _)
+
+      val classOpts = classNames.map(db.findClass)
+      for (classOpt <- classOpts)
+        yield for (klass <- classOpt)
+          yield klass + signaturesProp
+
+      ()
+    }
+
   override def migrations: Seq[Migration] = Seq (
-    Migration(0, migration0)
+    Migration(0, migration0),
+    Migration(1, migration1)
   )
 }
