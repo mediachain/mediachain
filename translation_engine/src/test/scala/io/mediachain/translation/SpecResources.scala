b758bbf67ce2c4fc44fa709bfc75ba93d935b4e8
Arkadiy Kukarkin <arkadiy@mine.nyc>
2016-04-01 12:21:38 -0400

    Add Moma + Tate test subclasses

--- translation_engine/src/test/scala/io/mediachain/translation/SpecResources.scala
+++ translation_engine/src/test/scala/io/mediachain/translation/SpecResources.scala
@@ -20,13 +20,30 @@ object SpecResources {
       fixtureDir.exists && fixtureDir.isDirectory
     }
 
-    val sampleArtworks = List((new File(fixtureDir, "/artworks/a/000/a00001-1035.json") ->
+    val sampleArtworks = List(new File(fixtureDir, "/artworks/a/000/a00001-1035.json") ->
       ImageBlob(
        id = None,
        title = "A Figure Bowing before a Seated Old Man with his Arm Outstretched in Benediction. Verso: Indecipherable Sketch",
        description = "Watercolour, ink, chalk and graphite on paper. Verso: graphite on paper",
        date = "date not known",
        author = Some(Person(None, name = "Robert Blake")),
-       external_ids = Map("tate:id" -> "1035"))))
+       external_ids = Map("tate:id" -> "1035")))
     }
+
+  object Moma extends Partner {
+    lazy val fixtureDir = new File(resourceUrl("/datasets/moma").toURI)
+
+    def sampleDataExists = {
+      fixtureDir.exists && fixtureDir.isDirectory
+    }
+
+    val sampleArtworks = List(new File("/small-moma-sample.json") ->
+      ImageBlob(
+        id = None,
+        title = "Three Part Large Animals",
+        description = "Polyurethane foam and wire",
+        date = "1989",
+        author = Some(Person(None, name = "Bruce Nauman")),
+        external_ids = Map("moma:MoMANumber" -> "194.1996.a-c")))
+  }
 }
