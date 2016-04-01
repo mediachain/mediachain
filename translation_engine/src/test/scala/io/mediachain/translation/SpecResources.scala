1b2b11125296f20161f95e8f74fe6cbe97fc8c41
Arkadiy Kukarkin <arkadiy@mine.nyc>
2016-04-01 11:56:14 -0400

    Generalize translator tests
    - Sadly the case class macro matchers don't seem to want to just take
      another instance of that case class and match all props, possibly because it wouldn't know what type of matchers to use in each case (though I'd be ok with an overrideable _==)
    - The nested option/case class member is particularly sad :(
    - But otherwise, good!

--- translation_engine/src/test/scala/io/mediachain/translation/SpecResources.scala
+++ translation_engine/src/test/scala/io/mediachain/translation/SpecResources.scala
@@ -2,25 +2,31 @@ package io.mediachain.translation
 
 import java.io.File
 
+import io.mediachain.Types.{ImageBlob, Person}
+
 object SpecResources {
+  sealed abstract class Partner {
+    val sampleArtworks: List[(File, ImageBlob)]
+  }
+
   def resourceUrl(path: String) = this.getClass.getResource(path)
 
   lazy val simpleTestResourceUrl = resourceUrl("/simple-test.json")
 
-  object Tate {
+  object Tate extends Partner {
     lazy val fixtureDir = new File(resourceUrl("/datasets/tate").toURI)
 
     def sampleDataExists = {
       fixtureDir.exists && fixtureDir.isDirectory
     }
 
-    object SampleArtworkA00001 {
-      val jsonFile = new File(fixtureDir, "/artworks/a/000/a00001-1035.json")
-      val title = "A Figure Bowing before a Seated Old Man with his Arm Outstretched in Benediction. Verso: Indecipherable Sketch"
-      val medium = "Watercolour, ink, chalk and graphite on paper. Verso: graphite on paper"
-      val dateText = "date not known"
-      val artistName = "Robert Blake"
-      val tateId = "1035"
+    val sampleArtworks = List((new File(fixtureDir, "/artworks/a/000/a00001-1035.json") ->
+      ImageBlob(
+       id = None,
+       title = "A Figure Bowing before a Seated Old Man with his Arm Outstretched in Benediction. Verso: Indecipherable Sketch",
+       description = "Watercolour, ink, chalk and graphite on paper. Verso: graphite on paper",
+       date = "date not known",
+       author = Some(Person(None, name = "Robert Blake")),
+       external_ids = Map("tate:id" -> "1035"))))
     }
-  }
 }
--- translation_engine/src/test/scala/io/mediachain/translation/TateTranslatorSpec.scala
+++ translation_engine/src/test/scala/io/mediachain/translation/TateTranslatorSpec.scala
@@ -4,36 +4,77 @@ import io.mediachain.Types._
 import io.mediachain.XorMatchers
 import org.json4s.JObject
 import org.specs2.Specification
+import org.specs2.matcher.MatcherMacros
 import tate.{TateLoader, TateTranslator}
 import org.json4s.jackson.JsonMethods._
+// strangely this is needed even though we aren't *defining* any macros per se, for MatcherMacros
+import scala.language.experimental.macros
 
 import scala.io.Source
 
-object TateTranslatorSpec extends Specification with XorMatchers {
+abstract class TranslatorSpec extends Specification with XorMatchers with MatcherMacros {
+
+  val resources: SpecResources.Partner
 
   def is = skipAllUnless(SpecResources.Tate.sampleDataExists) ^
-  s2"""
+    s2"""
        $loadsArtwork - Translates Tate artwork json into ImageBlob
     """
 
   def loadsArtwork = {
-    val expected = SpecResources.Tate.SampleArtworkA00001
+    val (jsonFile, expected) = resources.sampleArtworks.head
 
-    if (!expected.jsonFile.exists) {
-      ok(s"Skipping artwork test for ${expected.jsonFile.getPath}. File does not exist")
+    if (!jsonFile.exists) {
+      ok(s"Skipping artwork test for ${jsonFile.getPath}. File does not exist")
     } else {
-      val source: String = Source.fromFile(expected.jsonFile).mkString
+      val source: String = Source.fromFile(jsonFile).mkString
       val json: JObject = parse(source).asInstanceOf[JObject] // have faith
 
       val translated = TateTranslator.translate(json)
 
       translated must beRightXor { blob: ImageBlob =>
-        (blob.title must_== expected.title) and
-          (blob.author.exists(_.name must_== expected.artistName) and
-            (blob.external_ids.get("tate:id") must beSome(expected.tateId)))
+        blob must matchA[ImageBlob]
+          .title(expected.title)
+          .date(expected.date)
+          .description(expected.description)
+          .author { authorO => authorO must beSome((_: Person) must matchA[Person].name(expected.author.get.name)) }
+          .external_ids(_ must havePairs(expected.external_ids.toList:_*))
+
+       // _ must matchA[Person].name(expected.author.name))
+//            .external_ids(expected.title)
+//        (blob.title must_== expected.title) and
+//          (blob.author.exists(_.name must_== expected.artistName) and
+//            (blob.external_ids.get("tate:id") must beSome(expected.tateId)))
       }
     }
   }
 }
+
+//object TateTranslatorSpec extends Specification with XorMatchers {
+//
+//  def is = skipAllUnless(SpecResources.Tate.sampleDataExists) ^
+//  s2"""
+//       $loadsArtwork - Translates Tate artwork json into ImageBlob
+//    """
+//
+//  def loadsArtwork = {
+//    val expected = SpecResources.Tate.SampleArtworkA00001
+//
+//    if (!expected.jsonFile.exists) {
+//      ok(s"Skipping artwork test for ${expected.jsonFile.getPath}. File does not exist")
+//    } else {
+//      val source: String = Source.fromFile(expected.jsonFile).mkString
+//      val json: JObject = parse(source).asInstanceOf[JObject] // have faith
+//
+//      val translated = TateTranslator.translate(json)
+//
+//      translated must beRightXor { blob: ImageBlob =>
+//        (blob.title must_== expected.title) and
+//          (blob.author.exists(_.name must_== expected.artistName) and
+//            (blob.external_ids.get("tate:id") must beSome(expected.tateId)))
+//      }
+//    }
+//  }
+//}
 
 // TODO: MoMA translator spec here too?
