9a1633ce61011a2d36e109e8733771343cfb3f0e
Yusef Napora <yusef@napora.org>
2016-04-06 14:40:44 -0400

    update ingress & translator code to use blob bundle
    - tests not compiling yet

--- l_space/src/main/scala/io/mediachain/Ingress.scala
+++ l_space/src/main/scala/io/mediachain/Ingress.scala
@@ -7,8 +7,50 @@ import core.GraphError._
 import gremlin.scala._
 
 object Ingress {
+
   import Traversals.{GremlinScalaImplicits, VertexImplicits}
   import io.mediachain.util.GremlinUtils._
+
+  def ingestBlobBundle(graph: Graph, bundle: BlobBundle
+    , rawMetadata: Option[RawMetadataBlob] = None)
+  : Xor[GraphError, Canonical] = {
+    withTransactionXor(graph) {
+      val addResultXor: Xor[GraphError, (Vertex, Canonical)] =
+        bundle.content match {
+          case image: ImageBlob => {
+            addImageBlob(graph, image)
+          }
+
+          case person: Person => {
+            addPerson(graph, person)
+          }
+
+          case _ => Xor.left(SubtreeError()) // TODO, better error type
+        }
+
+      addResultXor.flatMap { addResult =>
+        val (vertex, canonical) = addResult
+        rawMetadata.foreach(attachRawMetadata(vertex, _))
+
+        val relationshipXors = bundle.relationships.map {
+          case BlobBundle.Author(author) =>
+            addPerson(graph, author).flatMap { result =>
+              val (authorV, authorCanonical) = result
+
+              rawMetadata.foreach(attachRawMetadata(authorV, _))
+              defineAuthorship(vertex, authorCanonical)
+            }
+        }
+
+        relationshipXors.foldLeft(Xor.right[GraphError, Canonical](canonical)) {
+          case (Xor.Left(err), _) => Xor.left(err)
+          case (_, Xor.Left(err)) => Xor.left(err)
+          case (_, Xor.Right(_)) => Xor.right(canonical)
+        }
+      }
+    }
+  }
+
 
   def attachRawMetadata(blobV: Vertex, raw: RawMetadataBlob): Unit = {
     val graph = blobV.graph
@@ -41,45 +83,41 @@ object Ingress {
   }
 
   // throws?
-  def addPerson(graph: Graph,
-                author: Person,
-                raw: Option[RawMetadataBlob] = None):
-  Xor[GraphError, Canonical] = {
+  def addPerson(graph: Graph, person: Person):
+  Xor[GraphError, (Vertex, Canonical)] = {
     // If there's an exact match already, return it,
     // otherwise create a new Person vertex and canonical
     // and return the canonical
-    val q = Traversals.personBlobsWithExactMatch(graph.V, author)
+    val q = Traversals.personBlobsWithExactMatch(graph.V, person)
 
-    val personV: Vertex = q.headOption.getOrElse(graph + author)
+    val personV: Vertex = q.headOption.getOrElse(graph + person)
 
-    raw.foreach(attachRawMetadata(personV, _))
-
-    graph.V(personV.id)
+    val canonical = graph.V(personV.id)
       .findCanonicalXor
-      .orElse {
+      .getOrElse {
         val canonicalV = graph + Canonical.create()
         canonicalV --- DescribedBy --> personV
-        Xor.right(canonicalV.toCC[Canonical])
+        canonicalV.toCC[Canonical]
       }
+
+    Xor.right((personV, canonical))
   }
 
-  def addImageBlob(graph: Graph,
-                   photo: ImageBlob,
-                   raw: Option[RawMetadataBlob] = None):
-  Xor[GraphError, Canonical] = {
+  def addImageBlob(graph: Graph, image: ImageBlob):
+  Xor[GraphError, (Vertex, Canonical)] = {
 
-    val photoV = Traversals.imageBlobsWithExactMatch(graph.V, photo)
-        .headOption.getOrElse(graph + photo)
+    val imageV = Traversals.imageBlobsWithExactMatch(graph.V, image)
+        .headOption.getOrElse(graph + image)
 
-    raw.foreach(attachRawMetadata(photoV, _))
+    val canonical = graph.V(imageV.id)
+      .findCanonicalXor
+      .getOrElse {
+        val canonicalVertex = graph + Canonical.create
+        canonicalVertex --- DescribedBy --> imageV
+        canonicalVertex.toCC[Canonical]
+      }
 
-      graph.V(photoV.id)
-        .findCanonicalXor
-        .orElse {
-          val canonicalVertex = graph + Canonical.create
-          canonicalVertex --- DescribedBy --> photoV
-          Xor.right(canonicalVertex.toCC[Canonical])
-        }
+    Xor.right((imageV, canonical))
   }
 
 
