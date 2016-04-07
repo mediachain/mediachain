2655880d42f1304f62b3b9b2819e26b13ba406b8
Yusef Napora <yusef@napora.org>
2016-04-07 14:35:36 -0400

    catch exceptions when committing or rolling back transactions

--- l_space/src/main/scala/io/mediachain/util/GremlinUtils.scala
+++ l_space/src/main/scala/io/mediachain/util/GremlinUtils.scala
@@ -1,60 +1,73 @@
 package io.mediachain.util
 
 import cats.data.Xor
+import com.orientechnologies.orient.core.tx.ORollbackException
 import gremlin.scala._
 import io.mediachain.core.GraphError
 import io.mediachain.core.GraphError.TransactionFailed
 import org.apache.tinkerpop.gremlin.orientdb.OrientGraph
 
 object GremlinUtils {
-  def beginTx(graph: Graph): Unit = graph match {
+  def beginTx(graph: Graph): Xor[TransactionFailed, Unit] = graph match {
     case orientGraph: OrientGraph =>
-      orientGraph.getRawDatabase.begin()
-    case _ => graph.tx.open()
+      Xor.catchNonFatal {
+        orientGraph.getRawDatabase.begin()
+        ()
+      }.leftMap(TransactionFailed)
+    case _ => Xor.catchNonFatal(graph.tx.open())
+      .leftMap(TransactionFailed)
   }
 
-  def commitTx(graph: Graph): Unit = graph match {
+  def commitTx(graph: Graph): Xor[TransactionFailed, Unit] = graph match {
     case orientGraph: OrientGraph =>
-      orientGraph.getRawDatabase.commit()
-    case _ => graph.tx.commit()
+      Xor.catchNonFatal{
+        orientGraph.getRawDatabase.commit()
+        ()
+      }.leftMap(TransactionFailed)
+    case _ =>
+      Xor.catchNonFatal(graph.tx.commit())
+        .leftMap(TransactionFailed)
   }
 
-  def rollbackTx(graph: Graph): Unit = graph match {
+  def rollbackTx(graph: Graph): Xor[TransactionFailed, Unit] = graph match {
     case orientGraph: OrientGraph =>
-      orientGraph.getRawDatabase.rollback()
-    case _ => graph.tx.rollback()
+      Xor.catchNonFatal {
+        orientGraph.getRawDatabase.rollback()
+        ()
+      }.leftMap(TransactionFailed)
+    case _ => Xor.catchNonFatal(graph.tx.rollback())
+      .leftMap(TransactionFailed)
   }
 
-  def withTransaction[T](graph: Graph)(f: => T): Xor[GraphError, T] = {
+  def withTransaction[T](graph: Graph)(f: => T): Xor[GraphError, T] =
+    for {
+      _ <- beginTx(graph)
 
-    beginTx(graph)
+      resultXor = Xor.catchNonFatal(f).leftMap(TransactionFailed)
 
-    val result: Xor[GraphError, T] =
-      Xor.catchNonFatal(f).leftMap(TransactionFailed)
+      _ <- resultXor match {
+        case Xor.Left(_) => rollbackTx(graph)
+        case _ => commitTx(graph)
+      }
 
-    result match {
-      case Xor.Left(_) => rollbackTx(graph)
-      case _ => commitTx(graph)
-    }
-
-    result
-  }
+      result <- resultXor
+    }  yield result
 
 
   def withTransactionXor[T](graph: Graph)(f: => Xor[GraphError, T])
-  : Xor[GraphError, T] = {
-    beginTx(graph)
+  : Xor[GraphError, T] =
+    for {
+      _ <- beginTx(graph)
 
-    val result: Xor[GraphError, T] =
-      Xor.catchNonFatal(f)
+      resultXor = Xor.catchNonFatal(f)
         .leftMap(TransactionFailed)
         .flatMap(res => res)
 
-    result match {
-      case Xor.Left(er) => rollbackTx(graph)
-      case _ => commitTx(graph)
-    }
+      _ <- resultXor match {
+        case Xor.Left(_) => rollbackTx(graph)
+        case _ => commitTx(graph)
+      }
 
-    result
-  }
+      result <- resultXor
+    } yield result
 }
