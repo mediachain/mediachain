539f0ae507b84226a6217e583bebaf38ccbfe25d
Yusef Napora <yusef@napora.org>
2016-04-11 17:23:59 -0400

    rename HelloService -> LSpaceService

--- api_server/src/main/scala/io/mediachain/api/LSpaceServiceActor.scala
+++ api_server/src/main/scala/io/mediachain/api/LSpaceServiceActor.scala
@@ -9,13 +9,13 @@ import spray.httpx.Json4sJacksonSupport
 import gremlin.scala._
 import org.apache.tinkerpop.gremlin.orientdb.OrientGraphFactory
 
-object HelloServiceActor {
+object LSpaceServiceActor {
   def props(graphFactory: OrientGraphFactory): Props =
-    Props(classOf[HelloServiceActor], graphFactory)
+    Props(classOf[LSpaceServiceActor], graphFactory)
 }
 
-class HelloServiceActor(val graphFactory: OrientGraphFactory)
-  extends Actor with HelloService {
+class LSpaceServiceActor(val graphFactory: OrientGraphFactory)
+  extends Actor with LSpaceService {
 
   def actorRefFactory = context
 
@@ -26,7 +26,7 @@ object JsonSupport extends Json4sJacksonSupport {
   implicit def json4sJacksonFormats = DefaultFormats
 }
 
-trait HelloService extends HttpService {
+trait LSpaceService extends HttpService {
   import JsonSupport._
 
   val graphFactory: OrientGraphFactory
