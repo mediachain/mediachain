57444cd7815c77ffecba33a1429e3af4631f8a61
Yusef Napora <yusef@napora.org>
2016-04-11 18:07:39 -0400

    route to list canonicals & fetch by id

--- api_server/src/main/scala/io/mediachain/api/LSpaceServiceActor.scala
+++ api_server/src/main/scala/io/mediachain/api/LSpaceServiceActor.scala
@@ -1,13 +1,17 @@
 package io.mediachain.api
 
+import java.util.UUID
+
 import akka.actor.{Actor, Props}
 import spray.routing.HttpService
 import spray.http._
 import io.mediachain.Types._
-import org.json4s.DefaultFormats
+import org.json4s.{DefaultFormats, JObject}
 import spray.httpx.Json4sJacksonSupport
 import gremlin.scala._
+import io.mediachain.{Query, Traversals}
 import org.apache.tinkerpop.gremlin.orientdb.OrientGraphFactory
+import org.json4s.JsonAST.JString
 
 object LSpaceServiceActor {
   def props(graphFactory: OrientGraphFactory): Props =
@@ -19,7 +23,7 @@ class LSpaceServiceActor(val graphFactory: OrientGraphFactory)
 
   def actorRefFactory = context
 
-  def receive = runRoute(helloRoute)
+  def receive = runRoute(baseRoute)
 }
 
 object JsonSupport extends Json4sJacksonSupport {
@@ -38,18 +42,49 @@ trait LSpaceService extends HttpService {
     result
   }
 
-  val helloRoute =
-    path("") {
+
+  val PAGE_SIZE = 20
+  def listCanonicals(page: Int): List[Canonical] =
+    withGraph { graph =>
+      val first = page * PAGE_SIZE
+      val last = first + PAGE_SIZE
+
+      graph.V.hasLabel[Canonical].toCC[Canonical]
+        .range(first, last).toList
+    }
+
+  def canonicalWithID(canonicalID: UUID): Option[Canonical] =
+    withGraph { graph =>
+      Traversals.canonicalsWithUUID(graph.V, canonicalID)
+        .toCC[Canonical]
+        .headOption
+    }
+
+  def historyForCanonical(canonicalID: UUID): JObject = {
+    JObject(List("TODO" -> JString("Implement me!")))
+  }
+
+
+
+  val baseRoute =
+    pathPrefix("canonicals") {
       get {
-        complete {
-          withGraph { graph =>
-            graph.V
-              .hasLabel[Canonical]
-              .toCC[Canonical]
-              .headOption
-              .getOrElse(throw new IllegalStateException("Can't get canonical"))
+        // GET "/canonicals"
+        pathEnd {
+          parameter("page" ? 0) { page =>
+            complete(listCanonicals(page))
           }
-        }
+        } ~
+          pathPrefix(JavaUUID) { canonicalID: UUID =>
+            // GET "/canonicals/some-canonical-id"
+            pathEnd {
+              complete(canonicalWithID(canonicalID))
+            } ~
+              // GET "/canonicals/some-canonical-id/history
+              path("history") {
+                complete(historyForCanonical(canonicalID))
+              }
+          }
       }
     }
 }
