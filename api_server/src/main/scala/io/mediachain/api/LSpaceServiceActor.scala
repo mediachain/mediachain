cd6d0aff28914602f22764eb77adc3e11a3bbc01
Yusef Napora <yusef@napora.org>
2016-04-12 16:44:33 -0400

    separate api operations from route definition

--- api_server/src/main/scala/io/mediachain/api/LSpaceServiceActor.scala
+++ api_server/src/main/scala/io/mediachain/api/LSpaceServiceActor.scala
@@ -3,15 +3,13 @@ package io.mediachain.api
 import java.util.UUID
 
 import akka.actor.{Actor, Props}
+import cats.data.Xor
 import spray.routing.HttpService
 import spray.http._
-import io.mediachain.Types._
-import org.json4s.{DefaultFormats, JObject}
+import org.json4s.DefaultFormats
 import spray.httpx.Json4sJacksonSupport
 import gremlin.scala._
-import io.mediachain.{Query, Traversals}
 import org.apache.tinkerpop.gremlin.orientdb.OrientGraphFactory
-import org.json4s.JsonAST.JString
 
 object LSpaceServiceActor {
   def props(graphFactory: OrientGraphFactory): Props =
@@ -43,44 +41,65 @@ trait LSpaceService extends HttpService {
   }
 
 
-  val PAGE_SIZE = 20
-  def listCanonicals(page: Int): List[Canonical] =
-    withGraph { graph =>
-      val first = page * PAGE_SIZE
-      val last = first + PAGE_SIZE
-
-      graph.V.hasLabel[Canonical].toCC[Canonical]
-        .range(first, last).toList
-    }
-
-  def canonicalWithID(canonicalID: UUID): Option[Canonical] =
-    withGraph { graph =>
-      Traversals.canonicalsWithUUID(graph.V, canonicalID)
-        .toCC[Canonical]
-        .headOption
-    }
-
-  def historyForCanonical(canonicalID: UUID): JObject = ???
-
-  val baseRoute =
+  import operations.CanonicalQueries._
+  val canonicalRoutes =
     pathPrefix("canonicals") {
       get {
         // GET "/canonicals"
         pathEnd {
           parameter("page" ? 0) { page =>
-            complete(listCanonicals(page))
+            complete {
+              withGraph(listCanonicals(page))
+            }
           }
         } ~
           pathPrefix(JavaUUID) { canonicalID: UUID =>
             // GET "/canonicals/some-canonical-id"
             pathEnd {
-              complete(canonicalWithID(canonicalID))
+              complete {
+                withGraph(canonicalWithID(canonicalID))
+              }
             } ~
               // GET "/canonicals/some-canonical-id/history
               path("history") {
-                complete(historyForCanonical(canonicalID))
+                complete {
+                  withGraph(historyForCanonical(canonicalID))
+                }
               }
           }
       }
     }
+
+  import operations.Translation._
+  val translationRoutes =
+    pathPrefix("translate") {
+      post {
+        path(Segment) { partnerName: String =>
+          entity(as[String]) { rawMetadataString: String =>
+            complete {
+              val resultXor =
+                translateRawMetadata(partnerName, rawMetadataString)
+
+              resultXor match {
+                case Xor.Left(err) =>
+                  throw new RuntimeException(s"Translation error: $err")
+                case Xor.Right(result) => result
+              }
+            }
+          }
+        }
+      }
+    }
+
+  val ingestionRoutes =
+    pathPrefix("ingest") {
+      post {
+        complete(???)
+      }
+    }
+
+  val baseRoute =
+    canonicalRoutes ~
+      translationRoutes ~
+      ingestionRoutes
 }
