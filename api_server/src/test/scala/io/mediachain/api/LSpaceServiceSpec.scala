bfc64784c9b0930ee1b032f4ad29d78706e47d14
Arkadiy Kukarkin <arkadiy@mine.nyc>
2016-04-12 12:49:54 -0400

    Stub tests for remaining endpoints

--- api_server/src/test/scala/io/mediachain/api/LSpaceServiceSpec.scala
+++ api_server/src/test/scala/io/mediachain/api/LSpaceServiceSpec.scala
@@ -1,7 +1,8 @@
 package io.mediachain.api
 
+import java.util.UUID
 import io.mediachain.BaseSpec
-import io.mediachain.Types.Canonical
+import io.mediachain.Types._
 import org.specs2.specification.BeforeAll
 import spray.testkit.Specs2RouteTest
 import gremlin.scala._
@@ -13,21 +14,36 @@ object LSpaceServiceSpec extends BaseSpec
 
   val graphFactory = MigrationHelper.newInMemoryGraphFactory()
 
+  val canonicalId = UUID.randomUUID.toString
+
   def beforeAll: Unit = {
     val graph = graphFactory.getTx()
-    graph + Canonical.create
+    graph + Canonical(None, canonicalId)
   }
 
   def is =
     s2"""
        returns a canonical from GET "/" $returnsFirstCanonical
+       returns a canonical by ID $returnsACanonical
+       returns a canonical's rev history by ID $returnsASubtree
       """
-
 
   def returnsFirstCanonical = {
     Get("/canonicals") ~> baseRoute ~> check {
       responseAs[String] must contain("canonicalID")
     }
   }
+
+  def returnsACanonical = pending {
+    Get("/canonicals/" + canonicalId) ~> baseRoute ~> check {
+      responseAs[String] must contain("canonicalID")
+    }
+  }
+  def returnsASubtree = pending {
+    Get("/canonicals/" + canonicalId + "/history") ~> baseRoute ~> check {
+      responseAs[String] must contain("canonicalID")
+    }
+  }
+
 
 }
