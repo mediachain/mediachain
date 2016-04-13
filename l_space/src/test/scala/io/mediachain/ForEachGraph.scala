a9b970fae4e2122caa4a2ad1b0cb2063c3185768
Yusef Napora <yusef@napora.org>
2016-04-13 12:26:21 -0400

    reuse one OrientGraph for all examples in a spec

--- l_space/src/test/scala/io/mediachain/ForEachGraph.scala
+++ l_space/src/test/scala/io/mediachain/ForEachGraph.scala
@@ -1,13 +1,20 @@
 package io.mediachain
 
-import gremlin.scala.Graph
+import com.orientechnologies.orient.core.sql.OCommandSQL
+import gremlin.scala._
+import io.mediachain.util.GremlinUtils.withTransaction
 import io.mediachain.util.orient.MigrationHelper
+import org.apache.tinkerpop.gremlin.orientdb.OrientGraphFactory
 import org.specs2.execute.{AsResult, Failure, Result, Success}
 import org.specs2.specification.{Around, Context}
 import org.specs2.specification.core.Env
 import org.specs2.specification.create.{ContextualFragmentFactory, FragmentsFactory}
 
 trait ForEachGraph[T] extends FragmentsFactory { outer =>
+
+  protected lazy val graphFactory: OrientGraphFactory =
+    MigrationHelper.newInMemoryGraphFactory()
+
 
   protected def forEachGraph(graph: Graph): T
 
@@ -17,12 +24,19 @@ trait ForEachGraph[T] extends FragmentsFactory { outer =>
   }
 
   protected def withNewGraph[R: AsResult](f: T => R): R = {
-    val graph = MigrationHelper.newInMemoryGraph()
+    val graph = graphFactory.getTx
     try {
       val t = forEachGraph(graph)
       f(t)
     } finally {
-      graph.database.drop()
+      val db = graph.getRawDatabase
+      // about the UNSAFE here... normally you'd use
+      // DELETE VERTEX instead of plain DELETE, so that you're
+      // not left with disconnected edges.  But we want to wipe out
+      // everything from all vertex and edge classes, so we're not
+      // concerned about that.
+      db.command(new OCommandSQL("DELETE FROM V UNSAFE")).execute[Long]()
+      db.command(new OCommandSQL("DELETE FROM E UNSAFE")).execute[Long]()
     }
   }
 
