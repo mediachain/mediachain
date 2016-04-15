2f02816b2fab8cf1c6412ec299b72386056a8cfd
Yusef Napora <yusef@napora.org>
2016-04-15 13:28:01 -0400

    add |> and >> query chaining operators to GremlinScala

--- l_space/src/main/scala/io/mediachain/Traversals.scala
+++ l_space/src/main/scala/io/mediachain/Traversals.scala
@@ -6,61 +6,187 @@ import java.util.UUID
 import org.apache.tinkerpop.gremlin.process.traversal.step.util.BulkSet
 import shapeless.HNil
 import scala.collection.JavaConverters._
+import gremlin.scala._
+import Types._
+import core.GraphError
+import core.GraphError._
+import cats.data.Xor
+import shapeless.HList
 
 object Traversals {
-  import gremlin.scala._
-  import Types._
-  import core.GraphError
-  import core.GraphError._
-  import cats.data.Xor
-  import shapeless.HList
 
-  def canonicalsWithID[Labels <: HList]
-  (q: GremlinScala[Vertex, Labels], canonicalID: String, allowSuperseded: Boolean = false): GremlinScala[Vertex, Labels] =
+  object Implicits {
+
+    implicit class VertexQueryImplicits[InLabels <: HList]
+    (val gs: GremlinScala[Vertex, InLabels]) extends AnyVal
     {
-      val base =
-        q.hasLabel[Canonical]
-          .has(Canonical.Keys.canonicalID, canonicalID)
-      if (allowSuperseded) {
-        base
-      } else {
-        getCanonical(base)
-      }
+      /**
+        * chain together query pipeline operations.
+        * e.g.
+        *
+        * graph.V |> personsBlobsWithExactMatch(pablo) |> getCanonical
+        */
+      def |>[OutLabels <: HList]
+      (f: GremlinScala[Vertex, InLabels] => GremlinScala[Vertex, OutLabels]):
+      GremlinScala[Vertex, OutLabels] = f(gs)
+
+
+      /**
+        * apply a function to a query pipeline, returning some result
+        * e.g.
+        * val q = graph.V |> canonicalsWithID(someID)
+        * val vertexXor = q >> headXor
+        *
+        * or, if you don't mind the parens:
+        * val vertexXor = (graph.V |> canonicalsWithID(someID)) >> headXor
+        */
+      def >>[T](f: GremlinScala[Vertex, InLabels] => T): T = f(gs)
     }
 
 
+    implicit class VertexImplicits(v: Vertex) {
+      /**
+        * 'lift' a Vertex into a GremlinScala[Vertex, _] pipeline
+        *
+        * @return a query pipeline based on the vertex, or an InvalidElementId
+        *         error if the vertex has an invalid or temporary id.
+        *         This will happen if, e.g. the vertex was created during a
+        *         transaction that has not been committed yet.
+        */
+      def toPipeline: Xor[InvalidElementId, GremlinScala[Vertex, HNil]] = {
+        val idValidXor: Xor[InvalidElementId, Unit] = v.id match {
+          case orientId: ORecordId => {
+            if (orientId.isValid && (!orientId.isTemporary)) {
+              Xor.right({})
+            } else {
+              Xor.left(InvalidElementId())
+            }
+          }
+
+          case _ => Xor.right({})
+        }
+
+        idValidXor.map(_ => v.graph.V(v.id))
+      }
+    }
+  }
+
+
+  import Implicits._
+
+
+  def headXor[Labels <: HList]
+  (gs: GremlinScala[Vertex, Labels]): Xor[VertexNotFound, Vertex] =
+    Xor.fromOption(gs.headOption, VertexNotFound())
+
+
+  def typedHeadXor[L, R, Labels <: HList](ifNone: => L, fromVertex: Vertex => R)
+    (gs: GremlinScala[Vertex, Labels]): Xor[L, R] =
+    headXor(gs).bimap({_ => ifNone}, fromVertex)
+
+
+  def findCanonicalXor[Labels <: HList](gs: GremlinScala[Vertex, Labels])
+  : Xor[CanonicalNotFound, Canonical] =
+    (gs |> getCanonical) >> typedHeadXor(CanonicalNotFound(), _.toCC[Canonical])
+
+
+  def findAuthorXor[Labels <: HList](gs: GremlinScala[Vertex, Labels])
+  : Xor[CanonicalNotFound, Canonical] =
+    (gs |> getAuthor) >> typedHeadXor(CanonicalNotFound(), _.toCC[Canonical])
+
+
+  def findRawMetadataXor[Labels <: HList](gs: GremlinScala[Vertex, Labels])
+  : Xor[RawMetadataNotFound, RawMetadataBlob] =
+    (gs |> getRawMetadataForBlob) >>
+      typedHeadXor(RawMetadataNotFound(), _.toCC[RawMetadataBlob])
+
+
+  def findRootRevisionXor[Labels <: HList](gs: GremlinScala[Vertex, Labels])
+  : Xor[BlobNotFound, Vertex] =
+    (gs |> getRootRevision) >> typedHeadXor(BlobNotFound(), identity)
+
+
+  // can this fail, or just return an empty list?
+  // TODO: either handle errors or remove the Xor
+  def findWorksXor[Labels <: HList](gs: GremlinScala[Vertex, Labels])
+  : Xor[GraphError, List[Canonical]] =
+    Xor.right(
+      getWorks(gs)
+        .toCC[Canonical]
+        .toList
+    )
+
+
+  def findSubtreeXor[Labels <: HList](gs: GremlinScala[Vertex, Labels])
+  : Xor[SubtreeError, Graph] = {
+    val stepLabel = StepLabel[Graph]("subtree")
+    val result = getSubtree(gs, stepLabel)
+      .cap(stepLabel)
+      .headOption
+    Xor.fromOption(result, SubtreeError())
+  }
+
+
+  def canonicalsWithID[Labels <: HList]
+  (canonicalID: String, allowSuperseded: Boolean = false)
+    (q: GremlinScala[Vertex, Labels]): GremlinScala[Vertex, Labels] =
+  {
+    val base =
+      q.hasLabel[Canonical].has(Canonical.Keys.canonicalID, canonicalID)
+
+    if (allowSuperseded) base
+    else base |> getSupersedingCanonical
+  }
+
+
   def canonicalsWithUUID[Labels <: HList]
-  (q: GremlinScala[Vertex, Labels], canonicalID: UUID, allowSuperseded: Boolean = false): GremlinScala[Vertex, Labels] =
-    canonicalsWithID(q, canonicalID.toString.toLowerCase, allowSuperseded)
+  (canonicalID: UUID, allowSuperseded: Boolean = false)
+    (q: GremlinScala[Vertex, Labels]): GremlinScala[Vertex, Labels] =
+    q |> canonicalsWithID(canonicalID.toString.toLowerCase, allowSuperseded)
+
+
+  def getCanonical[Labels <: HList]
+  (gs: GremlinScala[Vertex, Labels]): GremlinScala[Vertex, Labels] =
+    gs.until(_.hasLabel[Canonical])
+      .repeat(
+        _.inE(ModifiedBy, DescribedBy)
+          .or(_.hasNot(Keys.Deprecated), _.hasNot(Keys.Deprecated, true))
+          .outV
+      )
+
 
   def personBlobsWithExactMatch[Labels <: HList]
-  (q: GremlinScala[Vertex, Labels], p: Person): GremlinScala[Vertex, Labels] =
+  (p: Person)(q: GremlinScala[Vertex, Labels])
+  : GremlinScala[Vertex, Labels] =
     q.hasLabel[Person]
       .has(Keys.MultiHash, p.multiHash.base58)
 
 
   def imageBlobsWithExactMatch[Labels <: HList]
-  (q: GremlinScala[Vertex, Labels], blob: ImageBlob): GremlinScala[Vertex, Labels] =
+  (blob: ImageBlob)(q: GremlinScala[Vertex, Labels])
+  : GremlinScala[Vertex, Labels] =
     q.hasLabel[ImageBlob]
       .has(Keys.MultiHash, blob.multiHash.base58)
 
 
   def rawMetadataBlobsWithExactMatch[Labels <: HList]
-  (q: GremlinScala[Vertex, Labels], raw: RawMetadataBlob)
+  (raw: RawMetadataBlob)(q: GremlinScala[Vertex, Labels])
   : GremlinScala[Vertex, Labels] =
     q.hasLabel[RawMetadataBlob]
       .has(Keys.MultiHash, raw.multiHash.base58)
 
+
   def describingOrModifyingBlobs[Labels <: HList]
-  (q: GremlinScala[Vertex, Labels], canonical: Canonical)
+  (canonical: Canonical)(q: GremlinScala[Vertex, Labels])
   : GremlinScala[Vertex, Labels] = {
-    canonicalsWithID(q, canonical.canonicalID)
+    canonicalsWithID(canonical.canonicalID)(q)
       .out(DescribedBy).aggregate("blobs")
       .until(_.not(_.out(ModifiedBy)))
       .repeat(_.out(ModifiedBy).aggregate("blobs"))
       .cap("blobs")
       .unfold[Vertex]
   }
+
 
   def getSupersedingCanonical[Labels <: HList]
   (gs: GremlinScala[Vertex, Labels])
@@ -74,21 +200,12 @@ object Traversals {
   def getAllCanonicalsIncludingSuperseded[Labels <: HList]
   (gs: GremlinScala[Vertex, Labels])
   : GremlinScala[Vertex, Labels] = {
-        getCanonical(gs).aggregate("canonicals")
-          .until(_.not(_.inE(SupersededBy)))
-          .repeat(_.in(SupersededBy).aggregate("canonicals"))
-          .cap("canonicals")
-          .unfold[Vertex]
+    getCanonical(gs).aggregate("canonicals")
+      .until(_.not(_.inE(SupersededBy)))
+      .repeat(_.in(SupersededBy).aggregate("canonicals"))
+      .cap("canonicals")
+      .unfold[Vertex]
   }
-
-  def getCanonical[Labels <: HList]
-  (gs: GremlinScala[Vertex, Labels]): GremlinScala[Vertex, Labels] =
-    gs.until(_.hasLabel[Canonical])
-      .repeat(
-      _.inE(ModifiedBy, DescribedBy)
-        .or(_.hasNot(Keys.Deprecated), _.hasNot(Keys.Deprecated, true))
-        .outV
-    )
 
 
   def getAuthor[Labels <: HList]
@@ -103,14 +220,16 @@ object Traversals {
     getSupersedingCanonical(base)
   }
 
+
   def getWorks[Labels <: HList]
   (gs: GremlinScala[Vertex, Labels]): GremlinScala[Vertex, Labels] = {
     val works =
       getAllCanonicalsIncludingSuperseded(gs)
-      .in(AuthoredBy)
+        .in(AuthoredBy)
 
     getCanonical(works)
   }
+
 
   def getRootRevision[Labels <: HList]
   (gs: GremlinScala[Vertex, Labels]): GremlinScala[Vertex, Labels] =
@@ -133,70 +252,10 @@ object Traversals {
     ).repeat(_.outE.subgraph(stepLabel).inV)
 
 
-  implicit class VertexImplicits(v: Vertex) {
-    /**
-      * 'lift' a Vertex into a GremlinScala[Vertex, _] pipeline
-      *
-      * @return a query pipeline based on the vertex, or an InvalidElementId
-      *         error if the vertex has an invalid or temporary id.
-      *         This will happen if, e.g. the vertex was created during a
-      *         transaction that has not been committed yet.
-      */
-    def toPipeline: Xor[InvalidElementId, GremlinScala[Vertex, HNil]] = {
-      val idValidXor: Xor[InvalidElementId, Unit] = v.id match {
-        case orientId: ORecordId => {
-          if (orientId.isValid && (!orientId.isTemporary)) {
-            Xor.right({})
-          } else {
-            Xor.left(InvalidElementId())
-          }
-        }
-
-        case _ => Xor.right({})
-      }
-
-      idValidXor.map(_ => v.graph.V(v.id))
-    }
-  }
-
-  implicit class GremlinScalaImplicits[Labels <: HList](gs: GremlinScala[Vertex, Labels]) {
-    private def traverseAndExtract[Err <: GraphError]
-    (f: GremlinScala[Vertex, Labels] => GremlinScala[Vertex, Labels])(otherwise: Err):
-    Xor[Err, Vertex] = {
-      val result = f(gs).headOption
-      Xor.fromOption(result, otherwise)
-    }
-
-    def findCanonicalXor: Xor[CanonicalNotFound, Canonical] =
-      traverseAndExtract(getCanonical) { CanonicalNotFound() }
-        .map(_.toCC[Canonical])
-
-    def findAuthorXor: Xor[CanonicalNotFound, Canonical] =
-      traverseAndExtract(getAuthor) { CanonicalNotFound() }
-        .map(_.toCC[Canonical])
-
-    // can this fail, or just return an empty list?
-    // TODO: either handle errors or remove the Xor
-    def findWorksXor: Xor[GraphError, List[Canonical]] =
-      Xor.right(
-        getWorks(gs)
-        .toCC[Canonical]
-        .toList
-      )
-
-    def findRawMetadataXor: Xor[RawMetadataNotFound, RawMetadataBlob] =
-      traverseAndExtract(getRawMetadataForBlob) { RawMetadataNotFound() }
-        .map(_.toCC[RawMetadataBlob])
-
-    def findRootRevision: Xor[BlobNotFound, Vertex] =
-      traverseAndExtract(getRootRevision) { BlobNotFound() }
-
-    def findSubtreeXor: Xor[SubtreeError, Graph] = {
-      val stepLabel = StepLabel[Graph]("subtree")
-      val result = getSubtree(gs, stepLabel)
-        .cap(stepLabel)
-        .headOption
-      Xor.fromOption(result, SubtreeError())
-    }
+  def usageExample(graph: Graph) = {
+    val bob = Person(None, "Bob Terwilliger")
+    graph.V |>
+      personBlobsWithExactMatch(bob) |>
+      getCanonical
   }
 }
