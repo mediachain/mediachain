7a771f892a3522b309863b31520bd24ac3dbf46f
Arkadiy Kukarkin <arkadiy@mindhole.org>
2016-04-15 14:21:45 -0400

    Enable ammonite for test:console

--- project/Build.scala
+++ project/Build.scala
@@ -22,7 +22,8 @@ object LSpaceBuild extends Build{
       "org.specs2" %% "specs2-junit" % "3.7" % "test",
       "org.specs2" %% "specs2-matcher-extra" % "3.7" % "test",
       "org.scalacheck" %% "scalacheck" % "1.13.0" % "test",
-      "com.github.scopt" %% "scopt" % "3.4.0"
+      "com.github.scopt" %% "scopt" % "3.4.0",
+      "com.lihaoyi" % "ammonite-repl" % "0.5.7" % "test" cross CrossVersion.full
     ),
     scalacOptions in Test ++= Seq("-Yrangepos")
   )
@@ -37,10 +38,7 @@ object LSpaceBuild extends Build{
 
   updateOptions := updateOptions.value.withCachedResolution(true)
 
-  lazy val l_space = project
-    .settings(scalaSettings: _*)
-    .dependsOn(orientdb_migrations)
-    .dependsOn(core)
+
 
   lazy val translation_engine = project
     .settings(scalaSettings: _*)
@@ -77,4 +75,61 @@ object LSpaceBuild extends Build{
     .aggregate(translation_engine, api_server)
   lazy val circle_2 = project
     .aggregate(core, l_space)
+
+  lazy val predef =  """
+  import com.orientechnologies.orient.core.Orient
+  import org.apache.tinkerpop.gremlin.orientdb.OrientGraphFactory
+  import gremlin.scala._
+  import io.mediachain.Types._
+  import io.mediachain.Traversals.{GremlinScalaImplicits, VertexImplicits}
+  import io.mediachain.util.orient.MigrationHelper
+
+  Orient.instance.removeShutdownHook()
+  lazy val graph = MigrationHelper.newInMemoryGraph()
+  """.split("\n").mkString("; ")
+  lazy val l_space = Project("l_space", file("l_space")).settings(scalaSettings ++ List(
+    version := "0.0.1-WORKPRINT",
+
+    scalaVersion := "2.11.7",
+
+    mainClass := Some("io.mediachain.LSpace"),
+
+    libraryDependencies ++= Seq(
+      "com.michaelpollmeier" % "gremlin-scala_2.11" % "3.1.1-incubating.1",
+      "com.michaelpollmeier" % "orientdb-gremlin" % "3.1.0-incubating.1",
+      "com.tinkerpop.blueprints" % "blueprints-core" % "2.6.0",
+      "org.typelevel" %% "cats" % "0.4.1",
+      "com.chuusai" %% "shapeless" % "2.2.5",
+      "org.json4s" %% "json4s-jackson" % "3.2.11",
+      "com.fasterxml.jackson.dataformat" % "jackson-dataformat-cbor" % "2.4.0",
+      "org.bouncycastle" % "bcprov-jdk15on" % "1.54",
+      "org.apache.directory.studio" % "org.apache.commons.codec" % "1.8"
+      ),
+
+    excludeDependencies ++= Seq(
+      SbtExclusionRule("commons-logging", "commons-logging"),
+      SbtExclusionRule("org.codehaus.groovy", "groovy"),
+      SbtExclusionRule("org.codehaus.groovy", "groovy-groovysh"),
+      SbtExclusionRule("org.codehaus.groovy", "groovy-console"),
+      SbtExclusionRule("org.codehaus.groovy", "groovy-templates"),
+      SbtExclusionRule("org.codehaus.groovy", "groovy-xml"),
+      SbtExclusionRule("org.codehaus.groovy", "groovy-swing"),
+      SbtExclusionRule("org.codehaus.groovy", "groovy-json"),
+      SbtExclusionRule("org.codehaus.groovy", "groovy-jsr223")
+      ),
+
+    // see http://stackoverflow.com/a/9901616
+    //
+    // instantiating the `SBTSetupHook` and `SBTCleanupHook`
+    // classes causes code to run that prepares the testing
+    // environment & works around some classloader issues with
+    // sbt and orient / gremlin
+    testOptions in Test += Tests.Setup( loader => {
+      println("test setup")
+      loader.loadClass("io.mediachain.SBTSetupHook").newInstance
+      }),
+
+    initialCommands in (Test, console) := "ammonite.repl.Main.run(\"" + predef + "\")"
+    )).dependsOn(orientdb_migrations)
+      .dependsOn(core)
 }
