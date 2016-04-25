e4572eeaa6d2ceb5db74c68e50162b79ca25a18b
vyzo <vyzo@hackzen.org>
2016-04-25 20:00:59 +0300

    minimal transactor interface

--- transactor/build.sbt
+++ transactor/build.sbt
@@ -1,5 +1,7 @@
 name := "transactor"
+scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq(
+ "org.typelevel" %% "cats" % "0.4.1"
 )
 
--- transactor/src/main/scala/io/mediachain/transactor/Types.scala
+++ transactor/src/main/scala/io/mediachain/transactor/Types.scala
@@ -1,7 +1,72 @@
 package io.mediachain.transactor
 
-/** General companion object for case classes/datatypes.
-  */
 object Types {
-
+  import cats.data.Xor
+  
+  // Mediachain Datastore Records
+  abstract class Record
+  
+  // References to records in the underlying datastore
+  abstract class Reference
+  
+  // Canonical records: Entities and Artefacts
+  abstract class CanonicalRecord extends Record {
+    def name: String
+    def meta: Map[String, Any]
+  }
+  
+  case class Entity(
+    name: String, 
+    meta: Map[String, Any]
+  ) extends CanonicalRecord
+  
+  case class Artefact( 
+    name: String,
+    meta: Map[String, Any]
+  ) extends CanonicalRecord
+  
+  // Chain Cells
+  abstract class ChainCell extends Record {
+    def chain: Option[Reference]
+    def meta: Map[String, Any]
+  }
+  
+  abstract class EntityChainCell extends ChainCell {
+    def entity: Reference
+  }
+  
+  abstract class ArtefactChainCell extends ChainCell {
+    def artefact: Reference
+  }
+  
+  // Journal Entries
+  abstract class JournalEntry {
+    def index: BigInt
+    def ref: Reference
+  }
+  
+  case class CanonicalEntry( 
+    index: BigInt,
+    ref: Reference
+  ) extends JournalEntry
+  
+  case class ChainEntry( 
+    index: BigInt,
+    ref: Reference,
+    chain: Reference,
+    chainPrevious: Option[Reference]
+  ) extends JournalEntry
+  
+  // Journal transactor interface
+  trait Journal {
+    def insert(rec: CanonicalRecord): Xor[JournalError, CanonicalEntry]
+    def update(ref: Reference, cell: ChainCell): Xor[JournalError, ChainEntry]
+    def lookup(ref: Reference): Option[Reference]
+  }
+  
+  trait JournalClient {
+    def updateJournal(entry: JournalEntry): Unit
+  }
+  
+  abstract class JournalError
 }
