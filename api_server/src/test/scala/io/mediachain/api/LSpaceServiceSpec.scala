d635f2c3e9559af9c960d1c57e31f6b6e9f81b3a
Yusef Napora <yusef@napora.org>
2016-04-25 12:56:21 -0400

    pull in merge endpoint from spray branch

--- api_server/src/test/scala/io/mediachain/api/LSpaceServiceSpec.scala
+++ api_server/src/test/scala/io/mediachain/api/LSpaceServiceSpec.scala
@@ -21,6 +21,7 @@ object LSpaceServiceSpec extends BaseSpec
        returns a canonical by ID $returnsACanonical
        returns a canonical's rev history by ID $returnsASubtree
        returns the works by an author $returnsWorks
+       merges two canonicals $mergesCanonicals
       """
 
   def returnsFirstCanonical = {
@@ -82,6 +83,25 @@ object LSpaceServiceSpec extends BaseSpec
           /("canonicalID" -> imageByDuplicateAuthorCanonicalID)
         )
       )
+    }
+  }
+
+
+  def mergesCanonicals = {
+    val imageCanonicalID = context.objects.imageBlobCanonical.canonicalID
+    val extraImageCanonical = context.objects.extraImageBlobCanonical
+    val extraImageCanonicalID = extraImageCanonical.canonicalID
+
+    Post(s"/merge/$extraImageCanonicalID/into/$imageCanonicalID") ~>
+      baseRoute ~> check {
+
+      val r = responseAs[String]
+      r aka "merged canonical id" must /("canonicalID" -> imageCanonicalID)
+
+      val graph = graphFactory.getTx
+      extraImageCanonical.vertex(graph) must beRightXor { v: Vertex =>
+        v.outE(SupersededBy).exists() must beTrue
+      }
     }
   }
 }
