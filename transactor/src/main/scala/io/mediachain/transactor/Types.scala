850d4f7a116cacc05f55c01fd203d2688c64623d
vyzo <vyzo@hackzen.org>
2016-04-26 09:57:55 +0300

    transactor.Types: Add algebraic datatype for CBOR/IPLD values and some model streamlining
    - CValue <- CString, CMap, CSeq
    - Record::meta for accessing an unwrapped map
    - Remove name from CanonicalRecords, field is irrelevant to the transactor

--- transactor/src/main/scala/io/mediachain/transactor/Types.scala
+++ transactor/src/main/scala/io/mediachain/transactor/Types.scala
@@ -3,32 +3,34 @@ package io.mediachain.transactor
 object Types {
   import cats.data.Xor
   
+  // Canonical|CBOR Values: IPLD datatypes
+  sealed abstract class CValue
+  case class CString(value: String) extends CValue
+  case class CMap(value: Map[String, CValue]) extends CValue
+  case class CSeq(value: Seq[CValue]) extends CValue
+
   // Mediachain Datastore Records
-  abstract class Record
+  abstract class Record {
+    def meta: Map[String, CValue]
+  }
   
   // References to records in the underlying datastore
   abstract class Reference
   
   // Canonical records: Entities and Artefacts
-  abstract class CanonicalRecord extends Record {
-    def name: String
-    def meta: Map[String, Any]
-  }
+  abstract class CanonicalRecord extends Record
   
   case class Entity(
-    name: String, 
-    meta: Map[String, Any]
+    meta: Map[String, CValue]
   ) extends CanonicalRecord
   
   case class Artefact( 
-    name: String,
-    meta: Map[String, Any]
+    meta: Map[String, CValue]
   ) extends CanonicalRecord
   
   // Chain Cells
   abstract class ChainCell extends Record {
     def chain: Option[Reference]
-    def meta: Map[String, Any]
   }
   
   abstract class EntityChainCell extends ChainCell {
