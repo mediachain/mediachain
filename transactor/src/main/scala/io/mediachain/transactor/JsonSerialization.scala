963582290d35a635f12159f74d343f89ca6f991d
Yusef Napora <yusef@napora.org>
2016-04-27 11:38:45 -0400

    rename IPLDReference to IPFSReference, add comments

--- transactor/src/main/scala/io/mediachain/transactor/JsonSerialization.scala
+++ transactor/src/main/scala/io/mediachain/transactor/JsonSerialization.scala
@@ -3,13 +3,22 @@ package io.mediachain.transactor
 import io.mediachain.transactor.Types._
 import org.json4s._
 
-case class IPLDReference(link: String) extends Reference
+/**
+  * Represents a 'multihash' link to an IPFS resource
+  *
+  * @param multihash - the base58 encoded string representation of an IPFS
+  *             multihash
+  */
+case class IPFSReference(multihash: String) extends Reference
 
 object JsonSerialization {
 
   /** Like json4s ShortTypeHints, but removes the outer class name for
     * inner classes.  So instead of e.g. `Types$Artefact`, produces
     * `Artefact`
+    *
+    * These hints are encoded in the `type` field of the serialized json
+    * representation.
     */
   private case class ShortTypeHintsIgnoreOuterClass(hints: List[Class[_]])
     extends TypeHints {
@@ -20,9 +29,12 @@ object JsonSerialization {
     def classFor(hint: String) = hints find (hintFor(_) == hint)
   }
 
+  /**
+    * List of classes to provide type hints for when serializing.
+    */
   private val shortTypeHints = ShortTypeHintsIgnoreOuterClass(List(
     // reference
-    classOf[IPLDReference],
+    classOf[IPFSReference],
 
     // canonical records
     classOf[Entity],
@@ -37,11 +49,19 @@ object JsonSerialization {
     classOf[ChainEntry]
   ))
 
-  private val referenceSerializer = FieldSerializer[IPLDReference](
-    FieldSerializer.renameTo("link", "@link"),
-    FieldSerializer.renameFrom("@link", "link")
+  /**
+    * Rename the `multihash` field to `@link`, to match the IPLD spec
+    */
+  private val referenceSerializer = FieldSerializer[IPFSReference](
+    FieldSerializer.renameTo("multihash", "@link"),
+    FieldSerializer.renameFrom("@link", "multihash")
   )
 
+  /**
+    * Tells json4s to use the type hints we specified, and to store them
+    * in the `type` field.  Also adds the field-renaming serializer for
+    * `IPFSReference`
+    */
   implicit val jsonFormats = new DefaultFormats {
     override val dateFormat = DefaultFormats.lossless.dateFormat
     override val typeHints = shortTypeHints
@@ -49,6 +69,9 @@ object JsonSerialization {
   } + referenceSerializer
 
 
+  // Below are conversion functions from transactor types to
+  // json4s `JObject`s.  We may want to write a better conversion
+  // interface/
 
   def toJObject(record: Record): JObject =
     Extraction.decompose(record).asInstanceOf[JObject]
@@ -56,7 +79,7 @@ object JsonSerialization {
   def toJObject(chainCell: ChainCell): JObject =
     Extraction.decompose(chainCell).asInstanceOf[JObject]
 
-  def toJObject(ref: IPLDReference): JObject =
+  def toJObject(ref: IPFSReference): JObject =
     Extraction.decompose(ref).asInstanceOf[JObject]
 
   def toJObject(journalEntry: JournalEntry): JObject =
