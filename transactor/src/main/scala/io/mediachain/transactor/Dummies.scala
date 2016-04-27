ab6c148ac27fafc73daad9cf09c647d4becdeb6c
vyzo <vyzo@hackzen.org>
2016-04-27 17:10:32 +0300

    add Dummies for simple in-process testing of state machine client-server communication

---
+++ transactor/src/main/scala/io/mediachain/transactor/Dummies.scala
@@ -0,0 +1,29 @@
+package io.mediachain.transactor
+
+import scala.collection.mutable.{Map => MMap, HashMap => MHashMap}
+
+object Dummies {
+  import io.mediachain.transactor.Types._
+  
+  class DummyReference(val num: Int) extends Reference {
+    override def equals(that: Any) = {
+      that.isInstanceOf[DummyReference] && 
+        this.num == that.asInstanceOf[DummyReference].num
+    }
+    override def hashCode = num
+    override def toString = "dummy@" + num
+  }
+  
+  class DummyStore extends Datastore {
+    var seqno = 0
+    val records: MMap[Reference, Record] = new MHashMap
+    
+    override def put(rec: Record): Reference = {
+      val ref = new DummyReference(seqno)
+      seqno += 1
+      records += (ref -> rec)
+      ref
+    }
+  }
+
+}
