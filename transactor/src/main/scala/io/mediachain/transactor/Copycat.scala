69130635d4301f3eb3a6ce881f8f4bd3a97ba25d
vyzo <vyzo@hackzen.org>
2016-05-02 10:22:42 +0300

    Update Journal client interface, implement with Copycat.Client

--- transactor/src/main/scala/io/mediachain/transactor/Copycat.scala
+++ transactor/src/main/scala/io/mediachain/transactor/Copycat.scala
@@ -1,16 +1,18 @@
 package io.mediachain.transactor
 
 import java.io.File
-import java.util.function.Supplier
+import java.util.function.{Consumer, Supplier}
 import io.atomix.copycat.client.CopycatClient
 import io.atomix.copycat.server.{CopycatServer, StateMachine => CopycatStateMachine}
 import io.atomix.copycat.server.storage.{Storage, StorageLevel}
 import io.atomix.catalyst.transport.{Address, NettyTransport}
 import io.atomix.catalyst.serializer.Serializer
 
+import scala.compat.java8.FutureConverters
+
 object Copycat {
-  import io.mediachain.transactor.StateMachine.JournalStateMachine
-  import io.mediachain.transactor.Types.Datastore
+  import io.mediachain.transactor.StateMachine._
+  import io.mediachain.transactor.Types._
 
   object Server {
     def build(address: Address, logdir: String, datastore: Datastore,
@@ -38,15 +40,61 @@ object Copycat {
     }
   }
   
+  class Client(client: CopycatClient) extends JournalClient {
+    private var listeners: Set[JournalListener] = Set()
+    
+    // Journal 
+    def insert(rec: CanonicalRecord) =
+      FutureConverters.toScala(client.submit(JournalInsert(rec)))
+
+    def update(ref: Reference, cell: ChainCell) =
+      FutureConverters.toScala(client.submit(JournalUpdate(ref, cell)))
+    
+    def lookup(ref: Reference) = 
+      FutureConverters.toScala(client.submit(JournalLookup(ref)))
+    
+    def currentBlock =
+      FutureConverters.toScala(client.submit(JournalCurrentBlock()))
+    
+    // JournalClient
+    def connect(address: String) {
+      client.connect(new Address(address)).join()
+    }
+    
+    def close() {
+      client.close().join()
+    }
+    
+    def listen(listener: JournalListener) {
+      if (listeners.isEmpty) {
+        listeners = Set(listener)
+        client.onEvent("journal-commit", 
+          new Consumer[JournalCommitEvent] { 
+            def accept(evt: JournalCommitEvent) {
+              listeners.foreach(_.onJournalCommit(evt.entry))
+            }
+        })
+        client.onEvent("journal-block", 
+          new Consumer[JournalBlockEvent] { 
+            def accept(evt: JournalBlockEvent) { 
+              listeners.foreach(_.onJournalBlock(evt.ref))
+            }
+        })
+      } else {
+        listeners += listener
+      }
+    }
+  }
+
   object Client {
-    def build(): CopycatClient = {
+    def build(): Client = {
       val client = CopycatClient.builder()
                     .withTransport(NettyTransport.builder()
                                     .withThreads(2)
                                     .build())
                     .build()
       Serializers.register(client.serializer)
-      client
+      new Client(client)
     }
     
   }
