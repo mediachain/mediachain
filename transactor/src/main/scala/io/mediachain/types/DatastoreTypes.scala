f9f023941c7e021f1b2a53c6c7a633061d25c417
Yusef Napora <yusef@napora.org>
2016-05-04 12:50:11 -0400

    make EntityChainCell & ArtefactChainCell regular classes (not case classes)

--- transactor/src/main/scala/io/mediachain/types/DatastoreTypes.scala
+++ transactor/src/main/scala/io/mediachain/types/DatastoreTypes.scala
@@ -1,12 +1,11 @@
 package io.mediachain.types
 
-import io.mediachain.multihash.MultiHash
-import io.mediachain.types.CborSerialization.{CborSerializable, MediachainTypes}
-import io.mediachain.types.TransactorTypes.{ArtefactChainReference, ChainReference, EntityChainReference}
-import io.mediachain.util.cbor.CborAST._
-
 
 object DatastoreTypes {
+  import io.mediachain.multihash.MultiHash
+  import io.mediachain.types.CborSerialization.{CborSerializable, MediachainTypes}
+  import io.mediachain.types.TransactorTypes.{ArtefactChainReference, ChainReference, EntityChainReference}
+  import io.mediachain.util.cbor.CborAST._
 
   // Base class of all objects storable in the Datastore
   sealed trait DataObject extends Serializable with CborSerializable
@@ -62,10 +61,10 @@ object DatastoreTypes {
     def chain: Option[Reference]
   }
 
-  case class EntityChainCell(
-    entity: Reference,
-    chain: Option[Reference],
-    meta: Map[String, CValue]
+  class EntityChainCell(
+    val entity: Reference,
+    val chain: Option[Reference],
+    val meta: Map[String, CValue]
   ) extends ChainCell {
     val mediachainType = Some(MediachainTypes.EntityChainCell)
 
@@ -76,10 +75,18 @@ object DatastoreTypes {
     }
   }
 
-  case class ArtefactChainCell(
-    artefact: Reference,
-    chain: Option[Reference],
-    meta: Map[String, CValue]
+  object EntityChainCell {
+    def apply(entity: Reference, chain: Option[Reference], meta: Map[String, CValue]): EntityChainCell =
+      new EntityChainCell(entity, chain, meta)
+
+    def unapply(cell: EntityChainCell): Option[(Reference, Option[Reference], Map[String, CValue])] =
+      Some((cell.entity, cell.chain, cell.meta))
+  }
+
+  class ArtefactChainCell(
+    val artefact: Reference,
+    val chain: Option[Reference],
+    val meta: Map[String, CValue]
   ) extends ChainCell {
     val mediachainType = Some(MediachainTypes.ArtefactChainCell)
 
@@ -88,6 +95,14 @@ object DatastoreTypes {
       val optionals = Map("chain" -> chain.map(_.toCbor))
       super.toCMapWithDefaults(defaults, optionals)
     }
+  }
+
+  object ArtefactChainCell {
+    def apply(artefact: Reference, chain: Option[Reference], meta: Map[String, CValue]): ArtefactChainCell =
+      new ArtefactChainCell(artefact, chain, meta)
+
+    def unapply(cell: ArtefactChainCell): Option[(Reference, Option[Reference], Map[String, CValue])] =
+      Some((cell.artefact, cell.chain, cell.meta))
   }
 
 
