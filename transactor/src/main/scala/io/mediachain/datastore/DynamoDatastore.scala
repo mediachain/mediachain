1f40e425bbd864778cf6c7942cb57f22ad7cf839
vyzo <vyzo@hackzen.org>
2016-05-06 10:45:54 +0300

    DynamoDatastore: implement chunking

--- transactor/src/main/scala/io/mediachain/datastore/DynamoDatastore.scala
+++ transactor/src/main/scala/io/mediachain/datastore/DynamoDatastore.scala
@@ -7,66 +7,156 @@ import java.nio.{ByteBuffer,BufferUnderflowException}
 import scala.collection.mutable.{Buffer, ArrayBuffer}
 import io.mediachain.multihash.MultiHash
 
+// TODO error handling
+//      queued eventual writes in the background with disk backing of 
+//       in-progress writes
+//      use batch writes for chunked puts
 class DynamoDatastore(table: String, creds: BasicAWSCredentials)
   extends BinaryDatastore with AutoCloseable {
   import scala.collection.JavaConversions._
-
+  
+  val chunkSize = 256 * 384 // 384 KB; DynamoDB has 400KB limit
+  val chunkTable = table + "Chunks"
+  
   val db = new AmazonDynamoDBClient(creds)
 
   override def put(key: MultiHash, value: Array[Byte]) {
-    val hashBytes = new AttributeValue()
-    hashBytes.setS(key.base58)
-    val objBytes = new AttributeValue()
-    objBytes.setB(ByteBuffer.wrap(value))
-
-    val data = Map(
-      "multihash" -> hashBytes,
-      "data"      -> objBytes
-    )
-
-    db.putItem(table, data)
+    if (value.length < chunkSize) {
+      putSimple(key, value)
+    } else {
+      putChunked(key, value)
+    }
   }
 
+  private def putSimple(key: MultiHash, value: Array[Byte]) {
+    val keyAttr = new AttributeValue
+    keyAttr.setS(key.base58)
+    
+    val dataAttr = new AttributeValue
+    dataAttr.setB(bytes2Buffer(value))
+    
+    val item = Map(
+      "multihash" -> keyAttr,
+      "data"      -> dataAttr
+    )
+    
+    db.putItem(table, item)
+  }
+
+  private def putChunked(key: MultiHash, value: Array[Byte]) {
+    val key58 = key.base58
+    val chunks = chunkCount(value.length)
+    val chunkIds = (1 to chunks).map { n => key58 + "#" + n }
+    
+    // this should use the BatchWriteItemRequest API (PITA alert)
+    chunkIds.zipWithIndex.foreach {
+      case (chunkId, index) => {
+        val keyAttr = new AttributeValue
+        keyAttr.setS(chunkId)
+
+        val dataAttr = new AttributeValue
+        dataAttr.setB(bytes2BufferChunk(value, index))
+        
+        val item = Map(
+          "chunkId" -> keyAttr,
+          "data" -> dataAttr
+        )
+        
+        db.putItem(chunkTable, item)
+      }
+    }
+    
+    val keyAttr = new AttributeValue
+    keyAttr.setS(key58)
+    
+    val chunkAttr = new AttributeValue
+    chunkAttr.setSS(chunkIds)
+    
+    val item = Map(
+      "multihash" -> keyAttr,
+      "chunks" -> chunkAttr
+      )
+    
+    db.putItem(table, item)
+  }
+
+  private def chunkCount(len: Int): Int = {
+    val div = len / chunkSize
+    val rem = len % chunkSize
+    if (rem > 0) {
+      div + 1
+    } else {
+      div
+    }
+  }
+  
+  private def bytes2Buffer(bytes: Array[Byte]) = {
+    val buf = ByteBuffer.allocate(bytes.length + 4)
+    buf.putInt(bytes.length)
+    buf.put(bytes)
+    buf
+  }
+  
+  private def bytes2BufferChunk(bytes: Array[Byte], chunk: Int) = {
+    val offset = chunk * chunkSize
+    val len = Math.min(chunkSize, bytes.length - offset)
+    val buf = ByteBuffer.allocate(len + 4)
+    buf.putInt(len)
+    buf.put(bytes, offset, len)
+    buf
+  }
+  
   override def close() {
     db.shutdown()
   }
 
   override def get(key: MultiHash): Option[Array[Byte]] = {
-    val hashBytes = new AttributeValue()
-    hashBytes.setS(key.base58)
-    val data = Map(
-      "multihash" -> hashBytes
-    )
-    val result = db.getItem(table, data)
-    val rdata = result.getItem.get("data")
+    val keyAttr = new AttributeValue
+    keyAttr.setS(key.base58)
     
-    if (rdata != null) {
-      Some(bufferBytes(rdata.getB))
+    val item = Map(
+      "multihash" -> keyAttr
+    )
+    val res = db.getItem(table, item).getItem  // yes, really.
+    
+    val data = res.get("data")
+    val chunks = res.get("chunks")
+    
+    if (data != null) {
+      Some(buffer2Bytes(data.getB))
+    } else if (chunks != null) {
+      Some(getChunks(chunks.getSS.toList))
     } else {
       None
     }
   }
   
-  // this is ugly, perhaps we should serialize len+payload
-  private def bufferBytes(ibuf: ByteBuffer): Array[Byte] = {
-    def getByte(): Option[Byte] = {
-      try {
-        Some(ibuf.get())
-      } catch {
-        case (e: BufferUnderflowException) => None
+  private def getChunks(chunkIds: List[String]) = {
+    val buf = new ArrayBuffer[Byte]
+    // this should use the BatchGetItemRequest API (PITA alert)
+    chunkIds.foreach { chunkId =>
+      val keyAttr = new AttributeValue
+      keyAttr.setS(chunkId)
+      
+      val item = Map(
+        "chunkId" -> keyAttr
+      )
+      val res = db.getItem(chunkTable, item).getItem // yes, please.
+      val data = res.get("data")
+      if (data != null) {
+        val bytes = buffer2Bytes(data.getB)
+        buf ++= bytes
+      } else {
+        throw new RuntimeException("Missing chunk: " + chunkId)
       }
     }
-
-    def getBytes(obuf: Buffer[Byte]): Array[Byte] = {
-      getByte() match {
-        case Some(byte) => {
-          obuf += byte
-          getBytes(obuf)
-        }
-        case None => obuf.toArray
-      }
-    }
-    
-    getBytes(new ArrayBuffer)
+    buf.toArray
+  }
+  
+  private def buffer2Bytes(buf: ByteBuffer) = {
+    val len = buf.getInt()
+    val data = new Array[Byte](len)
+    buf.get(data)
+    data
   }
 }
