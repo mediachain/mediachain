57f7c50f4b0c8aa334e9454b09b2111c35b8dd2a
Yusef Napora <yusef@napora.org>
2016-05-09 11:19:31 -0400

    factor out generating single cell for canonical record

--- protocol/src/test/scala/io/mediachain/protocol/JournalBlockGenerators.scala
+++ protocol/src/test/scala/io/mediachain/protocol/JournalBlockGenerators.scala
@@ -7,7 +7,42 @@ object JournalBlockGenerators {
 
 
   /**
+    * Generate a chain cell that references the given `CanonicalRecord`
+    *
+    * @param canonical either an `Entity` or `Artefact` to generate a cell for
+    * @return depending on the type of `CanonicalRecord` given, either a
+    *         subtype of `EntityChainCell` or `EntityArtefactCell`.
+    *         Note that the `chain` field of the returned cell will be
+    *         set to `None`
+    *
+    */
+  def genCellFor(canonical: CanonicalRecord): Gen[ChainCell] = {
+    canonical match {
+      case e: Entity => {
+        val entityGen = Gen.const(e)
+        Gen.oneOf(
+          genEntityUpdateCell(entityGen, genNilReference),
+          genEntityLinkCell(entityGen, genNilReference)
+        )
+      }
+
+      case a: Artefact => {
+        val artefactGen = Gen.const(a)
+        Gen.oneOf(
+          genArtefactUpdateCell(artefactGen, genNilReference),
+          genArtefactCreationCell(artefactGen, genNilReference),
+          genArtefactDerivationCell(artefactGen, genNilReference),
+          genArtefactOwnershipCell(artefactGen, genNilReference),
+          genArtefactReferenceCell(artefactGen, genNilReference)
+        )
+      }
+    }
+  }
+
+
+  /**
     * Generate a chain of cells for the given `CanonicalRecord`.
+    *
     * @param length number of cells to generate
     * @param canonical the `ref` for each cell.  The type of `CanonicalRecord`
     *                  given will determine whether `EntityChainCell`s or
@@ -19,26 +54,7 @@ object JournalBlockGenerators {
     *         `None`.
     */
   def genChain(length: Int, canonical: CanonicalRecord): Gen[List[ChainCell]] = {
-    val cellGen: Gen[ChainCell] = canonical match {
-      case e: Entity => {
-        val entityGen = Gen.const(e)
-        Gen.oneOf(
-          genEntityUpdateCell(entityGen),
-          genEntityLinkCell(entityGen)
-        )
-      }
-
-      case a: Artefact => {
-        val artefactGen = Gen.const(a)
-        Gen.oneOf(
-          genArtefactUpdateCell(artefactGen),
-          genArtefactCreationCell(artefactGen),
-          genArtefactDerivationCell(artefactGen),
-          genArtefactOwnershipCell(artefactGen),
-          genArtefactReferenceCell(artefactGen)
-        )
-      }
-    }
+    val cellGen: Gen[ChainCell] = genCellFor(canonical)
 
     for {
       cells <- Gen.listOfN(length, cellGen)
