e2c3cb1c449eb494143fb3f31645918367a00035
vyzo <vyzo@hackzen.org>
2016-05-10 12:59:36 +0300

    RocksDatastore: add getKeys

--- transactor/src/main/scala/io/mediachain/datastore/RocksDatastore.scala
+++ transactor/src/main/scala/io/mediachain/datastore/RocksDatastore.scala
@@ -1,6 +1,7 @@
 package io.mediachain.datastore
 
 import org.rocksdb._
+import scala.collection.mutable.ListBuffer
 import io.mediachain.multihash.MultiHash
 
 class RocksDatastore(path: String)
@@ -20,6 +21,17 @@ class RocksDatastore(path: String)
   
   def removeData(key: MultiHash) {
     db.remove(key.bytes)
+  }
+  
+  def getKeys(): List[MultiHash] = {
+    val keys = new ListBuffer[MultiHash]
+    val iter = db.newIterator
+    iter.seekToFirst
+    while (iter.isValid) {
+      MultiHash.fromBytes(iter.key).foreach {key => keys += key}
+      iter.next
+    }
+    keys.toList
   }
 }
 
