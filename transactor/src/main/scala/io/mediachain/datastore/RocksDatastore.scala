8cd407986652ff8b2221f50110803b17c8fa1544
vyzo <vyzo@hackzen.org>
2016-05-10 15:36:28 +0300

    checked RocksDB sources -- no outside synchronization necessary for concurrent access.

--- transactor/src/main/scala/io/mediachain/datastore/RocksDatastore.scala
+++ transactor/src/main/scala/io/mediachain/datastore/RocksDatastore.scala
@@ -9,31 +9,29 @@ class RocksDatastore(path: String)
   val db = RocksDB.open(path)
   
   override def putData(key: MultiHash, data: Array[Byte]) {
-    this.synchronized {db.put(key.bytes, data)}
+    db.put(key.bytes, data)
   }
   
   override def close() {
-    this.synchronized {db.close()}
+    db.close()
   }
   
   override def getData(key: MultiHash): Option[Array[Byte]] = 
-    this.synchronized {Option(db.get(key.bytes))}
+    Option(db.get(key.bytes))
   
   def removeData(key: MultiHash) {
-    this.synchronized {db.remove(key.bytes)}
+    db.remove(key.bytes)
   }
   
-  def getKeys(): List[MultiHash] = 
-    this.synchronized {
-      val keys = new ListBuffer[MultiHash]
-      val iter = db.newIterator
-      iter.seekToFirst
-      while (iter.isValid) {
-        MultiHash.fromBytes(iter.key).foreach {key => keys += key}
-        iter.next
-      }
-      keys.toList
+  def getKeys(): List[MultiHash] = {
+    val keys = new ListBuffer[MultiHash]
+    val iter = db.newIterator
+    iter.seekToFirst
+    while (iter.isValid) {
+      MultiHash.fromBytes(iter.key).foreach {key => keys += key}
+      iter.next
     }
-  
+    keys.toList
+  }
 }
 
