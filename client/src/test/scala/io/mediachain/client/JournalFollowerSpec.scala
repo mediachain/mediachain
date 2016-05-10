efe70c88402965c06a8d4c1cb9791beaecca40a7
Yusef Napora <yusef@napora.org>
2016-05-10 14:54:56 -0400

    add spec for updating existing JournalFollower with new block

--- client/src/test/scala/io/mediachain/client/JournalFollowerSpec.scala
+++ client/src/test/scala/io/mediachain/client/JournalFollowerSpec.scala
@@ -2,6 +2,7 @@ package io.mediachain.client
 
 import io.mediachain.BaseSpec
 import org.specs2.ScalaCheck
+import org.specs2.matcher.ContainWithResultSeq
 
 object JournalFollowerSpec extends BaseSpec with ScalaCheck {
   import io.mediachain.protocol.Datastore._
@@ -15,6 +16,9 @@ object JournalFollowerSpec extends BaseSpec with ScalaCheck {
           - fetches all previous blocks from datastore $fetchesAllBlocks
           - fetches all canonical records $fetchesAllCanonicals
           - fetches all chain cells $fetchesAllChainCells
+
+       given a bootstrapped JournalFollower:
+         - updates itself when a new block is added $updatesWithNewBlock
       """
 
   implicit val scalaCheckParams = Parameters(
@@ -54,5 +58,34 @@ object JournalFollowerSpec extends BaseSpec with ScalaCheck {
 
     val follower = bootstrappedFollower(chainWithStore)
     follower.chainCells must containTheSameElementsAs(allCells)
+  }
+
+  def updatesWithNewBlock = prop { chainWithStore: BlockChainWithDatastore =>
+    val follower = bootstrappedFollower(chainWithStore)
+    val newBlockWithStore =
+      genJournalBlock(5, chainWithStore.datastore, chainWithStore.blockChain.lastOption)
+      .sample.getOrElse(
+        throw new Exception("Unable to generate new block for mock mediachain")
+      )
+
+    // update the follower's datastore.  In a real-world scenario with a
+    // shared global datastore, this would be unnecessary
+    newBlockWithStore.datastore.store.foreach { pair =>
+      follower.datastore.put(pair._2)
+    }
+
+    val newBlockRef = MultihashReference.forDataObject(newBlockWithStore.block)
+    follower.onJournalBlock(newBlockRef)
+
+    val canonicalRefsInNewBlock = newBlockWithStore.block.entries.collect {
+      case e: CanonicalEntry => e.ref
+    }
+    val canonicalsInNewBlock = canonicalRefsInNewBlock.flatMap(
+      newBlockWithStore.datastore.getAs[CanonicalRecord]
+    )
+
+    canonicalsInNewBlock.length must_== canonicalRefsInNewBlock.length
+    follower.currentBlock must_== Some(newBlockWithStore.block)
+    follower.canonicals must containAllOf(canonicalsInNewBlock)
   }
 }
