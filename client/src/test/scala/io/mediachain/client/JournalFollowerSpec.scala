4ea22a48ce1ea5ecaa19e0f604b5ce59657c33d4
Yusef Napora <yusef@napora.org>
2016-05-10 13:29:31 -0400

    check that JournalFollower finds all canonicals, chain cells referenced in blockchain

--- client/src/test/scala/io/mediachain/client/JournalFollowerSpec.scala
+++ client/src/test/scala/io/mediachain/client/JournalFollowerSpec.scala
@@ -1,13 +1,13 @@
 package io.mediachain.client
 
 import io.mediachain.BaseSpec
-import io.mediachain.protocol.InMemoryDatastore
 import org.specs2.ScalaCheck
-import org.specs2.scalacheck.Parameters
 
 object JournalFollowerSpec extends BaseSpec with ScalaCheck {
   import io.mediachain.protocol.Datastore._
   import io.mediachain.protocol.JournalBlockGenerators._
+  import org.specs2.scalacheck.Parameters
+
 
   def is =
     s2"""
@@ -22,21 +22,37 @@ object JournalFollowerSpec extends BaseSpec with ScalaCheck {
     maxSize = 5
   )
 
-
-  def fetchesAllBlocks = prop { chainWithStore: BlockChainWithDatastore =>
+  private def bootstrappedFollower(chainWithStore: BlockChainWithDatastore): JournalFollower = {
     val follower = new JournalFollower(chainWithStore.datastore)
 
     val chainHead = chainWithStore.blockChain.lastOption
     val chainHeadRef = chainHead.map(MultihashReference.forDataObject)
     chainHeadRef.foreach(follower.bootstrapJournal)
-
-
-    follower.currentBlock must_== chainHead
-
-    follower.sortedBlocks must containTheSameElementsAs(chainWithStore.blockChain)
+    follower
   }
 
-  def fetchesAllCanonicals = pending
+  def fetchesAllBlocks = prop { chainWithStore: BlockChainWithDatastore =>
+    val follower = bootstrappedFollower(chainWithStore)
 
-  def fetchesAllChainCells = pending
+    follower.currentBlock must_== chainWithStore.blockChain.lastOption
+    follower.blocks must containTheSameElementsAs(chainWithStore.blockChain)
+  }
+
+  def fetchesAllCanonicals = prop { chainWithStore: BlockChainWithDatastore =>
+    val allCanonicals = chainWithStore.datastore.store.values.collect {
+      case c: CanonicalRecord => c
+    }.toList
+
+    val follower = bootstrappedFollower(chainWithStore)
+    follower.canonicals must containTheSameElementsAs(allCanonicals)
+  }
+
+  def fetchesAllChainCells = prop { chainWithStore: BlockChainWithDatastore =>
+    val allCells = chainWithStore.datastore.store.values.collect {
+      case c: ChainCell => c
+    }.toList
+
+    val follower = bootstrappedFollower(chainWithStore)
+    follower.chainCells must containTheSameElementsAs(allCells)
+  }
 }
