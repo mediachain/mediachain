7543d0c13548ee1d879e7fe5e42113253db91316
Yusef Napora <yusef@napora.org>
2016-05-10 13:30:18 -0400

    add client subproject to mediachain aggregate

--- project/Build.scala
+++ project/Build.scala
@@ -164,6 +164,6 @@ object MediachainBuild extends Build {
   // dependsOn means classes will be available
   lazy val mediachain = (project in file("."))
     .aggregate(core, l_space,
-      translation_engine, rpc, transactor, protocol)
+      translation_engine, rpc, transactor, protocol, client)
 }
 
