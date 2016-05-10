542bdd0c7b1a285ed52635165855f3049a890b80
Yusef Napora <yusef@napora.org>
2016-05-10 14:32:56 -0400

    don't keep all blocks in memory in JournalFollower, just keep refs

--- client/src/main/scala/io/mediachain/client/JournalFollower.scala
+++ client/src/main/scala/io/mediachain/client/JournalFollower.scala
@@ -4,63 +4,98 @@ import io.mediachain.protocol.Datastore._
 import io.mediachain.protocol.Transactor.JournalListener
 
 class JournalFollower(datastore: Datastore) extends JournalListener {
-  import collection.mutable.{Set => MSet}
+  import collection.mutable.{Set => MSet, Map => MMap}
 
-  private val blockSet: MSet[JournalBlock] = MSet()
-  def blocks = blockSet.toList.sortBy(_.index)
+  override def onJournalCommit(entry: JournalEntry): Unit = {
+    // TODO
+  }
+
+  override def onJournalBlock(ref: Reference): Unit =
+    handleNewBlock(ref)
+
+
+  private val blockRefs: MSet[Reference] = MSet()
+  private def blockSet: Set[JournalBlock] =
+    blockRefs.toSet.flatMap(datastore.getAs[JournalBlock])
+
+  def blocks: List[JournalBlock] = blockSet.toList.sortBy(_.index)
   def currentBlock: Option[JournalBlock] = blocks.lastOption
 
+  // Map of canonical reference to most recent ChainEntry
+  // for that canonical
+  private val chainHeads: MMap[Reference, ChainEntry] = MMap()
+
+  /// Set of all `CanonicalRecord`s referenced in the blockchain
   def canonicals: Set[CanonicalRecord] =
-    blockSet.toSet.flatMap { block: JournalBlock =>
+    blockSet.flatMap { block: JournalBlock =>
       val refs = block.entries.collect {
-        case e: CanonicalEntry => e.ref
+       case e: CanonicalEntry => e.ref
       }
-      refs.flatMap(getCanonical)
+      refs.flatMap(datastore.getAs[CanonicalRecord])
     }
 
 
+  /// Set of all chain cells in the block chain.
   def chainCells: Set[ChainCell] =
-    blockSet.toSet.flatMap { block: JournalBlock =>
+    blockSet.flatMap { block: JournalBlock =>
       val refs = block.entries.collect {
         case e: ChainEntry => e.chain
       }
-      refs.flatMap(getChainCell)
+      refs.flatMap(datastore.getAs[ChainCell])
     }
 
+
+  /** reconstruct the blockchain from a reference to the current block
+    *
+    * @param currentBlockRef reference to the most recently committed
+    *                        `JournalBlock`
+    */
   def bootstrapJournal(currentBlockRef: Reference): Unit =
     onJournalBlock(currentBlockRef)
 
-  def getBlock(ref: Reference): Option[JournalBlock] =
-    datastore.get(ref).collect { case b: JournalBlock => b }
 
-  def getCanonical(ref: Reference): Option[CanonicalRecord] =
-    datastore.get(ref).collect { case c: CanonicalRecord => c}
+  /**
+    * Get the most recent `ChainCell` that's been applied to the referenced
+    * `CanonicalRecord`.
+    * @param canonicalRef reference to a `CanonicalRecord`
+    * @return the most recently applied `ChainCell`, or None if none exist
+    */
+  def getChainHeadForCanonical(canonicalRef: Reference): Option[ChainCell] =
+    chainHeads.get(canonicalRef)
+      .flatMap(entry => datastore.getAs[ChainCell](entry.chain))
 
-  def getChainCell(ref: Reference): Option[ChainCell] =
-    datastore.get(ref).collect { case c: ChainCell => c}
 
-  private def catchupPriorBlocks(currentBlock: JournalBlock): Unit = {
-    for {
-      prevRef <- currentBlock.chain
-      prevBlock <- getBlock(prevRef)
-    } yield {
-      if (!blockSet.contains(prevBlock)) {
-        blockSet.add(prevBlock)
-        catchupPriorBlocks(prevBlock)
+  private def updateChainHeads(chainEntries: Seq[ChainEntry]): Unit = {
+    chainEntries.foreach { entry =>
+      val latest = chainHeads.get(entry.ref) match {
+        case Some(existing) if existing.index >= entry.index => existing
+        case _ => entry
       }
+      chainHeads.put(entry.ref, latest)
     }
   }
 
-  override def onJournalCommit(entry: JournalEntry): Unit = {
 
+  private def catchupPriorBlocks(block: JournalBlock): Unit = {
+    block.chain
+      .foreach { prevBlockRef =>
+        if (!blockRefs.contains(prevBlockRef)) {
+          handleNewBlock(prevBlockRef)
+        }
+      }
   }
 
-  override def onJournalBlock(ref: Reference): Unit = {
-    val blockOpt: Option[JournalBlock] = getBlock(ref)
 
-    blockOpt.foreach { block =>
-      blockSet.add(block)
+  private def handleNewBlock(blockRef: Reference): Unit = {
+    datastore.getAs[JournalBlock](blockRef).foreach { block =>
+      blockRefs.add(blockRef)
+      val chainEntries = block.entries.toSeq
+        .collect { case e: ChainEntry => e }
+
+      updateChainHeads(chainEntries)
       catchupPriorBlocks(block)
     }
   }
+
+
 }
