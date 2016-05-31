b5f31c7855aa38e58a88d4e9c9df5373f6eed43c
Yusef Napora <yusef@napora.org>
2016-05-31 11:28:54 -0400

    add insert / update rpc endpoints

--- transactor/src/main/scala/io/mediachain/copycat/TransactorService.scala
+++ transactor/src/main/scala/io/mediachain/copycat/TransactorService.scala
@@ -1,12 +1,15 @@
 package io.mediachain.copycat
 
+import cats.data.Xor
 import io.grpc.{Status, StatusRuntimeException}
 import io.mediachain.multihash.MultiHash
-import io.mediachain.protocol.Datastore.{Datastore, MultihashReference, Reference}
-import io.mediachain.protocol.transactor.Transactor.TransactorServiceGrpc
+import io.mediachain.protocol.CborSerialization
+import io.mediachain.protocol.Datastore._
+import io.mediachain.protocol.Transactor.JournalError
+import io.mediachain.protocol.transactor.Transactor.{InsertRequest, TransactorServiceGrpc, UpdateRequest}
 import io.mediachain.protocol.transactor.Transactor
 
-import scala.concurrent.{Future, ExecutionContext}
+import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration.{Duration, SECONDS}
 
 class TransactorService(client: Client,
@@ -27,11 +30,81 @@ class TransactorService(client: Client,
       .lookup(ref)
       .map { (x: Option[Reference]) =>
         x.map { ref =>
-          Transactor.MultihashReference(new String(ref.toCborBytes))
+          refToRPCMultihashRef(ref)
         }.getOrElse {
           throw new StatusRuntimeException(Status.NOT_FOUND)
         }
       }
+  }
+
+  private def refToRPCMultihashRef(ref: Reference)
+  : Transactor.MultihashReference = ref match {
+    case MultihashReference(multihash) =>
+      Transactor.MultihashReference(multihash.base58)
+    case r =>
+      throw new ClassCastException(
+        s"Expected MultihashReference, got type ${r.getClass.getTypeName}"
+      )
+  }
+
+  override def insertCanonicalRecord(request: InsertRequest)
+  : Future[Transactor.MultihashReference] = {
+    val recXor = CborSerialization.fromCborBytes[CanonicalRecord](
+      request.canonicalCbor.toByteArray
+    )
+
+    val future = recXor match {
+      case Xor.Left(err) => throw new StatusRuntimeException(
+        Status.FAILED_PRECONDITION.withDescription(
+          s"Object deserialization error: ${err.message}"
+        )
+      )
+      case Xor.Right(obj) =>
+        client.insert(obj)
+    }
+
+    future.map { entryXor: Xor[JournalError, CanonicalEntry] =>
+      entryXor match {
+        case Xor.Left(err) =>
+          throw new StatusRuntimeException(
+            Status.FAILED_PRECONDITION.withDescription(
+              s"Journal Error: $err"
+            )
+          )
+        case Xor.Right(entry) =>
+          refToRPCMultihashRef(entry.ref)
+      }
+    }
+  }
+
+  override def updateCanonicalRecord(request: UpdateRequest)
+  : Future[Transactor.MultihashReference] = {
+    val cellXor = CborSerialization.fromCborBytes[ChainCell](
+      request.chainCellCbor.toByteArray
+    )
+
+    val future = cellXor match {
+      case Xor.Left(err) => throw new StatusRuntimeException(
+        Status.FAILED_PRECONDITION.withDescription(
+          s"Object deserialization error: ${err.message}"
+        )
+      )
+      case Xor.Right(cell) =>
+        client.update(cell.ref, cell)
+    }
+
+    future.map { entryXor: Xor[JournalError, ChainEntry] =>
+      entryXor match {
+        case Xor.Left(err) =>
+          throw new StatusRuntimeException(
+            Status.FAILED_PRECONDITION.withDescription(
+              s"Journal Error: $err"
+            )
+          )
+        case Xor.Right(entry) =>
+          refToRPCMultihashRef(entry.chain)
+      }
+    }
   }
 }
 
