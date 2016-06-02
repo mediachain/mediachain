765f1d76eb71fc2dba4411abc71fe85c719b3fd3
Yusef Napora <yusef@napora.org>
2016-06-02 13:37:44 -0400

    bump grpc version

--- project/Build.scala
+++ project/Build.scala
@@ -91,7 +91,7 @@ object MediachainBuild extends Build {
         version in PB.protobufConfig := "3.0.0-beta-2",
 
         libraryDependencies ++= Seq(
-          "io.grpc" % "grpc-all" % "0.9.0",
+          "io.grpc" % "grpc-all" % "0.14.0",
           "com.trueaccord.scalapb" %% "scalapb-runtime-grpc" %
             (PB.scalapbVersion in PB.protobufConfig).value
         )
