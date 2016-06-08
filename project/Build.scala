7a7af6e9abe281b83a31ee1ea20ba57242c3db55
vyzo <vyzo@hackzen.org>
2016-06-08 16:34:59 +0300

    transactor: add s3 dependency, bump aws sdk version to current

--- project/Build.scala
+++ project/Build.scala
@@ -51,7 +51,8 @@ object MediachainBuild extends Build {
         "org.slf4j" % "slf4j-api" % "1.7.21",
         "org.slf4j" % "slf4j-simple" % "1.7.21",
         "org.rocksdb" % "rocksdbjni" % "4.5.1",
-        "com.amazonaws" % "aws-java-sdk-dynamodb" % "1.10.74"
+        "com.amazonaws" % "aws-java-sdk-dynamodb" % "1.11.7",
+        "com.amazonaws" % "aws-java-sdk-s3" % "1.11.7"
       ),
       assemblyMergeStrategy in assembly := {
         case "META-INF/io.netty.versions.properties" => 
