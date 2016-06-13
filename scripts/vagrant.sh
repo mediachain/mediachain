810e0f8c0c17a239e9416584c30d2fcd521943a2
Yusef Napora <yusef@napora.org>
2016-06-13 13:01:39 -0400

    don't use $PWD in vagrant script
    - allows calling the script from directories other than the project
      root

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -2,6 +2,10 @@
 
 type vagrant >/dev/null 2>&1 || { echo >&2 "Please install vagrant: https://www.vagrantup.com/"; exit 1; }
 
-export VAGRANT_CWD=${PWD}/provisioning
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_ROOT="$SCRIPT_DIR/.."
+
+export VAGRANT_CWD="${PROJECT_ROOT}/provisioning"
 
 vagrant $@
