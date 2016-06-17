d5ff8a03053dad32455960f7cced3d02b3cf3319
Arkadiy Kukarkin <arkadiy@mine.nyc>
2016-06-17 14:13:07 -0400

    RIP l_space

--- l_space/src/test/scala/io/mediachain/BaseSpec.scala
+++
@@ -1,8 +0,0 @@
-package io.mediachain
-
-import org.specs2.Specification
-import org.specs2.matcher.ThrownExpectations
-
-trait BaseSpec extends Specification
-  with XorMatchers
-  with ThrownExpectations
